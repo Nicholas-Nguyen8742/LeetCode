3a2b5733c3d098e049a2b4452599eafe7a825cf5
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-15 17:30:59 -0500

    Time: 155 ms (55.49%), Space: 50 MB (13.46%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,32 +3,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const map = {
-        I: 1,
-        IV: 4,
-        V: 5,
-        IX: 9,
-        X: 10,
-        XL: 40,
-        L: 50,
-        XC: 90,
-        C: 100,
-        CD: 400,
-        D: 500,
-        CM: 900,
-        M: 1000
-    };
-    const len = s.length;
-    let i = 0;
-    let res = 0;
-    while (i < len) {
-        map[s.substr(i, 2)] 
-            ? res += map[s.substr(i, 2)]
-            : res += map[s[i]];
-        map[s.substr(i, 2)] 
-            ? i += 2
-            : i += 1;
+    const map = new Map([['I', 1],['IV', 4],['V', 5],['IX', 9],['X', 10],['XL', 40],['L', 50],['XC', 90],['C', 100],['CD', 400],['D', 500],['CM', 900],['M', 1000]]);
+
+    let count = 0;
+    let index = 0;
+    while (index < s.length) {
+      if (map.has(s.substr(index, 2))) {
+        count += map.get(s.substr(index, 2));
+        index += 2;
+      } else {
+       count += map.get(s[index]);
+       index += 1;
+      }      
     }
-    return res;
+    return count;
 
 };
