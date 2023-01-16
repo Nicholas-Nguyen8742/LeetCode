5268edaf2eaff261f8b51ed7e6bbbef4bb2437c4
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-15 19:11:56 -0500

    Time: 91 ms (62.83%), Space: 47.7 MB (19.02%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    var obj = nums.reduce((r, e) => (r[e] = (r[e] || 0) + 1, r), {});
-    var uniq = Object.keys(obj).filter(e => obj[e] == 1).map(Number);
-    return uniq;
+    const map = new Map();
+    nums.map((el) => map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1));
+    for (let entry of map) if (entry[1] === 1) return entry[0];
 };
