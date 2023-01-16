786fe00223c60ff28635064d857591c610798417
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-15 19:24:56 -0500

    Time: 62 ms (96.59%), Space: 44.7 MB (34.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -6,6 +6,5 @@ var majorityElement = function(nums) {
     const map = new Map();
     nums.map((el) => map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1));
     const keys = [...map.keys()];
-    const index = [...map.values()].indexOf(Math.max(...map.values()));
-    return keys[index];
+    return keys[[...map.values()].indexOf(Math.max(...map.values()))];
 };
