96d77ef2dfd7c0906de81d6ed9426aa385a8c341
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-17 09:48:28 -0500

    Time: 70 ms (88.33%), Space: 44.3 MB (54.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,9 +3,5 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let expectedSum = nums.length*(nums.length + 1)/2
-    let actualSum = nums.reduce((a, b) => a + b, 0)
-    let missingNumber = expectedSum - actualSum
-
-    return missingNumber 
+    return (nums.length*(nums.length + 1)/2) - (nums.reduce((a, b) => a + b, 0));
 };
