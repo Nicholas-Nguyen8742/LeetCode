091af34fa1e3601ac59646e6be1f78d7985412aa
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-23 10:11:21 -0500

    Time: 96 ms (51.94%), Space: 43.9 MB (86.22%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+  const clean = paragraph.replace(/,/g, ' ').replace(/[&\/\\#;,!+()$~%.'":*?<>{}]/g, '').toLowerCase().split(' ');
+  const without = clean.filter((element) => element === ""
+                               ? element
+                               : !banned.includes(element));
+  const map = without.reduce((acc, val) => {
+      if (acc.has(val)){
+         acc.set(val, acc.get(val) + 1);
+      } else {
+         acc.set(val, 1);
+      };
+      return acc;
+   }, new Map);
+   const frequencyArray = Array.from(map);
+   return frequencyArray.sort((a, b) => b[1] - a[1])[0][0];
+};
