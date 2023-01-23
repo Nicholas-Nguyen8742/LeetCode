cc2be5bb67f4e9338828066a06065fa124c81019
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-01-23 11:34:20 -0500

    Time: 63 ms (73.83%), Space: 41.3 MB (99.33%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+    const value = (new Date(Date.UTC(year, month - 1, day))).toUTCString().split(" ")[0].slice(0, -1);
+    const map = {
+        "Sun": "Sunday",
+        "Mon": "Monday",
+        "Tue": "Tuesday",
+        "Wed": "Wednesday",
+        "Thu": "Thursday",
+        "Fri": "Friday",
+        "Sat": "Saturday"
+    }
+
+    return map[value];
+};
