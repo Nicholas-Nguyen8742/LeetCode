76f4d89169c6a953d8a3c73a358e5ed811079e66
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-08-05 19:35:27 -0500

    Time: 45 ms (95.03%), Space: 41.5 MB (80.47%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,12 @@
+Array.prototype.last = function() {
+    return !this.length ? -1 : this[this.length - 1];
+};
+
+// Array.prototype.last = function() {
+//     if (!this.length) return -1;
+//     return this[this.length - 1]
+// };
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
