cea707d47373b33ae7bca446dfe414c57cd7d229
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-08-08 17:12:17 -0500

    Time: 263 ms (98.89%), Space: 107.1 MB (67.95%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const map = new Map();
+    return function(...args) {
+        const key = JSON.stringify(args);
+        return map.has(key)
+            ? map.get(key)
+            : map.set(key, fn(...args)).get(key);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
