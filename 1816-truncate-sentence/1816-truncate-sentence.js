2ccb2ae3208bba2b019b690b4a1a067880373762
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-10 13:19:25 -0400

    Time: 56 ms (37.86%), Space: 41.8 MB (74.01%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -4,6 +4,5 @@
  * @return {string}
  */
 var truncateSentence = function(s, k) {
-    const arr = s.split(' ').slice(0, k);
-    return arr.join(' ');
+    return s.split(' ').slice(0, k).join(' ');
 };
