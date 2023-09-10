cd5ab6a1d37961b14e8197f3909533f5796e879b
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-10 12:59:29 -0400

    Time: 312 ms (66.53%), Space: 118.7 MB (40.79%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    const map = new Map();
+    [...arr1, ...arr2].forEach((el) => {
+        map.has(el.id) ? map.set(el.id, { ...map.get(el.id), ...el }) : map.set(el.id, el);
+    });
+    return [...map.values()].sort((a, b) => a.id - b.id);
+};
