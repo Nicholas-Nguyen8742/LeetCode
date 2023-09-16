f14e93f83777f8312ed378ddf50ac727e992aa1b
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-16 11:11:50 -0400

    Time: 75 ms (5.39%), Space: 41.8 MB (70.44%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let curr = 0, next = 1;
+    while (true) {
+        yield curr;
+        [curr, next] = [next, curr + next];
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
