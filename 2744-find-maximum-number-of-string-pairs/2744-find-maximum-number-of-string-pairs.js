ec281ed0787f0a81df28649f54a2036c079618a1
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-15 22:03:19 -0400

    Time: 71 ms (50.08%), Space: 45 MB (49.11%) - LeetHub

---
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maximumNumberOfStringPairs = function(words) {
+    let pairs = 0, i = 0;
+    let arr = [];
+    while (i < words.length) {
+        let temp = words[i];
+        let word = temp.charAt(1) + temp.charAt(0);
+        if (arr.includes(word)) {
+            pairs += 1;
+        }
+        arr.push(words[i]);
+        i++;
+    }
+    return pairs;
+};
