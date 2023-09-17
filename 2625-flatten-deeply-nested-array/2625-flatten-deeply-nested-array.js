1aee891868f67f2e8ad5ba05071601946925968c
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-17 18:42:28 -0400

    Time: 152 ms (37.89%), Space: 102.4 MB (21.74%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -6,10 +6,6 @@
 var flat = function (arr, n) {
     if (n === 0) return arr;
     let result = []; 
-    arr.forEach((el) => {
-      Array.isArray(el)
-        ? result.push(...flat(el, n - 1))
-        : result.push(el);
-    });
+    arr.forEach((el) => Array.isArray(el) ? result.push(...flat(el, n - 1)) : result.push(el));
     return result;
 };
