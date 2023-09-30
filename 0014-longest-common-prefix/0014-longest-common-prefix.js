93e1526ab55c24e980226b4debf063c872978665
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-30 18:45:06 -0500

    Time: 67 ms (8.95%), Space: 42 MB (75.99%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,13 +3,12 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let stop = false;
     let string = '';
     for (let i = 0; i < strs[0].length; i++) {
-      if (!(Array.from({ length: strs.length }, (x, index) => strs[0].charAt(i) === strs[index].charAt(i)).includes(false)) && !stop) {
+      if (!(Array.from({ length: strs.length }, (x, index) => strs[0].charAt(i) === strs[index].charAt(i)).includes(false))) {
         string += strs[0].charAt(i);
       } else {
-        stop = true;
+        return string;
       }
     }
     return string;
