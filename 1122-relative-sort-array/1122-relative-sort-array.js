5a11a76e45d38b10fd64aceb2c7d7fc57be75c62
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-30 16:37:29 -0500

    Time: 56 ms (54.20%), Space: 43.3 MB (34.35%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    const filtered = arr1.filter((el) => !arr2.includes(el)).sort((a, b) => a - b);
-    const filled = arr2.map((el) => arr1.filter((dupe) => dupe === el)).flat(1);
-    return [...filled, ...filtered];
+    const filtered = arr1.filter((el) => !arr2.includes(el));
+    const filled = arr2.map((el) => arr1.filter((dupe) => dupe === el));
+    return [...filled.flat(1), ...filtered.sort((a, b) => a - b)];
 };
