fa10f408606f3f8858b249ac540cfe16ee9e1ca0
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-09-30 17:58:16 -0500

    Time: 60 ms (43.95%), Space: 45.4 MB (8.07%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.js
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.js
@@ -3,10 +3,11 @@
  * @return {string[]}
  */
 var stringMatching = function(words) {
-    return words.sort((a, b) => a.length - b.length).filter((el) => {
-       const temp = words.filter((word) => word.search(el) !== -1);
+    return words.sort((a, b) => a.length - b.length).reduce((acc, curr) => {
+      const temp = words.filter((word) => word.search(curr) !== -1);
        if (temp.length > 1) {
-         return temp[0];
+          acc.push(temp[0])
        }
-    });
+      return acc;
+    }, []);
 };
