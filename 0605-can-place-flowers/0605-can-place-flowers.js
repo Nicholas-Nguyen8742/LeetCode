828eb9b9ba73289ae99b83babb934c7a987d4f4f
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-05 01:26:41 -0600

    Time: 64 ms (23.91%), Space: 44.4 MB (45.84%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -6,10 +6,13 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let result = 0, i = 0;
     while (i < flowerbed.length) {
-        if (((flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0 && flowerbed[i] === 0) ||
-             (flowerbed[i - 1] === undefined && flowerbed[i + 1] === 0 && flowerbed[i] === 0) ||
-             (flowerbed[i - 1] === undefined && flowerbed[i + 1] === undefined && flowerbed[i] === 0) ||
-             (flowerbed[i + 1] === undefined && flowerbed[i - 1] === 0  && flowerbed[i] === 0))) {
+        const curr = flowerbed[i];
+        const before = flowerbed[i - 1];
+        const after = flowerbed[i + 1];
+        if (((before === 0 && after === 0 && curr === 0) ||
+             (before === undefined && after === 0 && curr === 0) ||
+             (before === undefined && after === undefined && curr === 0) ||
+             (after === undefined && before === 0  && curr === 0))) {
             flowerbed[i] = 1;
             result ++;
         }
