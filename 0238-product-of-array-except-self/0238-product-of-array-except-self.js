fa70b6a66748e5a97041b9a0d573e4f70134cad0
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-05 11:04:47 -0600

    Time: 10298 ms (5.02%), Space: 56.6 MB (11.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,10 +4,8 @@
  */
 var productExceptSelf = function(nums) {
     const copy = [];
-    let i = 0;
-    do {
-        copy[i] = nums.reduce((acc, curr, index) => index !== i ? acc * curr : acc, 1);
-        i++;
-    } while (i < nums.length);
+    for (const element of nums) {
+        copy.push(nums.reduce((acc, curr, index) => copy.length !== index ? acc * curr : acc, 1));
+    }
     return copy;
 };
