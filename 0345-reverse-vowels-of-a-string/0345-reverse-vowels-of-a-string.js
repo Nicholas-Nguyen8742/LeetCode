2b941da435ea493083dff880d56c859ecaee82af
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-05 09:38:36 -0600

    Time: 85 ms (22.30%), Space: 55.4 MB (9.25%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -5,15 +5,23 @@
 var isMatch = function(el, i) {
   const regex = /[aeiou]/gi;
   return el.match(regex) ? [el, i] : el;
-}
+};
 
 var isArray = function(el) {
   return Array.isArray(el);
+};
+
+var stringToMatchArr = function(string) {
+    return string.split('').map(isMatch);
 }
 
+var filterArrReverse = function(arr) {
+    return arr.filter(isArray).reverse();
+};
+
 var reverseVowels = function(s) {
-    const arr = s.split('').map(isMatch);
-    const filter = arr.filter(isArray).reverse();
+    const arr = stringToMatchArr(s);
+    const filter = filterArrReverse(arr);
     for (let i = filter.length - 1, j = 0; i >= 0; i--, j++)  arr[filter[j][1]] = filter[i][0];
     return arr.join('');
 };
