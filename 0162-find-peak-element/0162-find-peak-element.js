a795c1b32e81c4888d9b1ea52f10c12237d156ce
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 22:13:33 -0600

    Time: 54 ms (38.13%), Space: 42 MB (62.43%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -9,9 +9,13 @@ var isMatch = function(el, i, nums) {
            return el;
        }
    }
-}
+};
+
+var notMatch = function(nums) {
+    return nums[0] >= nums[nums.length - 1] ? 0 : nums.length - 1;
+};
 
 var findPeakElement = function(nums) {
     const found = nums.findIndex((el, i) => isMatch(el, i, nums));
-    return found === -1 ? (nums[0] >= nums[nums.length -1] ? 0 : nums.length - 1) : found;
+    return found === -1 ? (notMatch(nums)) : found;
 };
