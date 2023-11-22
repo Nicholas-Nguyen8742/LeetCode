6e7f2bb3e8a5b6739689edb7ec1f738bec78ad12
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 21:13:25 -0600

    Time: 56 ms (78.87%), Space: 44 MB (59.08%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let map = {};
+    let i = 0;
+    let length = nums.length;
+    while (i < length) {
+        const curr = nums[i];
+        if (!map[curr]) {
+            map[curr] = 1;
+            i++;
+        } else if (map[curr] === 1) {
+            map[curr] = 2;
+            i++;
+        } else if (map[nums[i]] === 2) {
+            nums.splice(i, 1);
+            length = length - 1;
+        }
+    }
+};
