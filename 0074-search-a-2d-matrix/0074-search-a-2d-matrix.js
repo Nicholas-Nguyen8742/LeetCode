a32f69839bc151126c23c6ff4d70ed948e1d2d86
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 21:50:11 -0600

    Time: 53 ms (50.88%), Space: 42.1 MB (43.72%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for (let row = 0; row < matrix.length; row++){
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (matrix[row][col] === target) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
