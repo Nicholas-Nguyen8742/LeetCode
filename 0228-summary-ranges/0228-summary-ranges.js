f08be419456447f268bde25db52ee6f15ef5afe3
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 23:35:21 -0600

    Time: 54 ms (32.66%), Space: 41.7 MB (77.93%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -8,7 +8,7 @@ var summaryRanges = function(nums) {
     let temp = [nums[0]];
     while (i <= nums.length) {
         const tempLength = temp.length;
-        const curr = nums[i]
+        const curr = nums[i];
         if (tempLength && (temp[tempLength - 1] + 1 !== curr)) {
             const val = tempLength === 1 ? `${temp[0]}` : `${temp[0]}->${temp[tempLength - 1]}`;
             res.push(val);
