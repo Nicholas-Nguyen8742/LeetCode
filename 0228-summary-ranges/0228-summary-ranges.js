6653af05a3185fc4266f22aac178aab31f57a87c
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 23:26:01 -0600

    Time: 63 ms (6.76%), Space: 41.6 MB (90.71%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -10,16 +10,17 @@ var summaryRanges = function(nums) {
         if (i === 0) {
             temp.push(nums[i]);
         } else {
-          if (temp.length && (temp[temp.length - 1] + 1 !== nums[i])) {
-              if (temp.length === 1) {
+          if (temp.length && (temp[temp.length - 1] + 1 === nums[i])) {
+            temp.push(nums[i]);
+
+          } else {
+            if (temp.length === 1) {
                 res.push(`${temp[0]}`);
               } else {
                 res.push(`${temp[0]}->${temp[temp.length - 1]}`);
               }
               temp = [];
               temp.push(nums[i]);
-          } else {
-            temp.push(nums[i]);
           }
         }
         i++;
