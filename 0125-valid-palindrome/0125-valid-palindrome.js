c0679e2b08332675669fb78552462fc80dc297ba
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-11-21 22:24:15 -0600

    Time: 68 ms (36.70%), Space: 47.8 MB (28.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,11 +3,14 @@
  * @return {boolean}
  */
 
+var cleanString = function(string) {
+  return string.replace(/[^a-z0-9]+/gi, '').toLowerCase();
+};
+
 var reverseString = function(string) {
     return string.split('').reverse().join('');
 };
 
 var isPalindrome = function(s) {
-    const string = s.replace(/[^a-z0-9]+/gi, '').toLowerCase();
-    return string === reverseString(string);
+    return cleanString(s) === reverseString(cleanString(s));
 };
