aef76b3efe15f4d3296963090c7ac35e272d2965
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-08-10 15:29:40 -0500

    Time: 154 ms (78.55%), Space: 62.6 MB (28.92%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.js
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -19,31 +19,6 @@ var MyHashMap = function() {
   })
 };
 
-// /** 
-//  * @param {number} key 
-//  * @param {number} value
-//  * @return {void}
-//  */
-// MyHashMap.prototype.put = function(key, value) {
-//   this.map[key] = value;
-// };
-
-// /** 
-//  * @param {number} key
-//  * @return {number}
-//  */
-// MyHashMap.prototype.get = function(key) {
-//   return this.map[key] == null ? -1 : this.map[key];
-// };
-
-// /** 
-//  * @param {number} key
-//  * @return {void}
-//  */
-// MyHashMap.prototype.remove = function(key) {
-//   delete this.map[key];
-// };
-
 /** 
  * Your MyHashMap object will be instantiated and called as such:
  * var obj = new MyHashMap()
