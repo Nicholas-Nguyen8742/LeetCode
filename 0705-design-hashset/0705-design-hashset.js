4f258188ad140c21a7a970d69a59ccecdf0b4678
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-08-10 16:07:28 -0500

    Time: 141 ms (79.71%), Space: 59.8 MB (91.69%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.js
+++ 0705-design-hashset/0705-design-hashset.js
@@ -1,31 +1,68 @@
 
 var MyHashSet = function() {
-  this.hash = Object.create(null);
+  return Object.create(null, {
+    size: {
+      value: 9137,
+    },
+    arr: {
+      value: new Array(this.size),
+    },
+    add: {
+      value: function(key) {
+        const index = key % this.size;
+        if (!this.arr[index]) {
+          this.arr[index] = [key]
+        } else {
+          if (!this.arr[index].includes(key)) {
+            this.arr[index].push(key)
+          }
+        }
+      },
+    },
+    remove: {
+      value: function(key) {
+        const index = key % this.size;
+        if (this.arr[index] && this.arr[index].includes(key)) {
+          const found = this.arr[index].findIndex((item) => item === key);
+          if (found !== -1) {
+            this.arr[index].splice(found, 1);
+          }
+        }
+        
+      },
+    },
+    contains: {
+      value: function(key) {
+        const index = key % this.size;
+        return !!(this.arr[index] && this.arr[index].includes(key));
+      },
+    },
+  });
 };
 
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.add = function(key) {
-    this.hash[key] = key;
-};
+// /** 
+//  * @param {number} key
+//  * @return {void}
+//  */
+// MyHashSet.prototype.add = function(key) {
+    
+// };
 
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function(key) {
-    delete this.hash[key];
-};
+// /** 
+//  * @param {number} key
+//  * @return {void}
+//  */
+// MyHashSet.prototype.remove = function(key) {
+    
+// };
 
-/** 
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function(key) {
-    return key in this.hash;
-};
+// /** 
+//  * @param {number} key
+//  * @return {boolean}
+//  */
+// MyHashSet.prototype.contains = function(key) {
+    
+// };
 
 /** 
  * Your MyHashSet object will be instantiated and called as such:
