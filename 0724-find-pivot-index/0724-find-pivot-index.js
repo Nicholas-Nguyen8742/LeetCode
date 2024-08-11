d32f20de9912f5e871493e15f3444c8ffa56025a
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-08-11 14:51:01 -0500

    Time: 574 ms (8.30%), Space: 58.7 MB (5.07%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -11,21 +11,32 @@ var pivotIndex = function(nums) {
   let left = -1;
   const leftArr = [];
   
-  let i = 0;
-  
-  while (i < nums.length) {
+  for (let i = 0; i < nums.length; i++) {
     const rightArr = nums.slice(i);
     rightArr.shift();
     let leftSum = addSum(leftArr);
     let rightSum = addSum(rightArr);
     if (leftSum === rightSum) {
-      left = i;
-      break;
+      return i;
     }
 
     leftArr.push(nums[i]);
-    i++;
   }
+//   let i = 0;
+  
+//   while (i < nums.length) {
+//     const rightArr = nums.slice(i);
+//     rightArr.shift();
+//     let leftSum = addSum(leftArr);
+//     let rightSum = addSum(rightArr);
+//     if (leftSum === rightSum) {
+//       left = i;
+//       break;
+//     }
+
+//     leftArr.push(nums[i]);
+//     i++;
+//   }
   
   
   return left;
