895669ffb110e9ac9788d367db157a85435d7592
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-08-11 15:04:04 -0500

    Time: 72 ms (39.43%), Space: 52.8 MB (26.42%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -2,23 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-var addSum = function(arr) {
-  return arr.reduce((a, b) => a + b, 0);
-}
-
 var pivotIndex = function(nums) {
   let leftSum = 0;
-  let rightSum = addSum(nums);
-  
+  let rightSum = nums.reduce((a, b) => a + b, 0);
   for (let i = 0; i < nums.length; i++) {
     if (leftSum === rightSum - nums[i]) {
       return i;
     }
- 
     leftSum += nums[i];
     rightSum -= nums[i];
   }
-  
   return -1;
 };
