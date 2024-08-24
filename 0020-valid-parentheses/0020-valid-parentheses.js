e8c861ecf07afc2b94886d6f578757749691f27f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-08-24 11:28:37 -0500

    Time: 46 ms (96.57%), Space: 54 MB (7.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,29 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function(s, unmatched = '') {
   const hash = {
     "(": ")",
     "{": "}",
     "[": "]"
   };
   
-  let unmatched = '';
-  
   let i = 0;
   while (i < s.length) {
     const unmatchedElIndex = unmatched.length - 1;
     const unmatchedEl = unmatched[unmatchedElIndex];
-    if (
-      unmatchedEl in hash
-      && s[i] === hash[unmatchedEl]
-    ) {
+    if (unmatchedEl in hash && s[i] === hash[unmatchedEl]) {
       unmatched = unmatched.substring(0, unmatchedElIndex);
     } else {
       unmatched += s[i];
     }
     i++;
   }
-  
+
   return !unmatched.length;
 };
