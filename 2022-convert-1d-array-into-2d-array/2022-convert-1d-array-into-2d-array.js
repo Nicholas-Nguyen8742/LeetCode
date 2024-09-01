6293375ce8eab488e234e9857ec2d63fdf7aaf1b
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-01 10:43:42 -0500

    Time: 182 ms (54.82%), Space: 72 MB (34.52%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
@@ -9,14 +9,12 @@ var construct2DArray = function(original, m, n) {
     return [];
   }
 
-  let currArray = [[]];
+  let currArray = Array.from(Array(m), () => []);
   let rowIndex = 0;
   let columnIndex = 0;
 
   for (let i = 0; i < original.length; i++) {
-    currArray[rowIndex] === undefined
-      ? currArray.push([original[i]])
-      : currArray[rowIndex].push(original[i]);
+    currArray[rowIndex].push(original[i]);
 
     if (currArray[rowIndex].length === n) {
       columnIndex = 0;
