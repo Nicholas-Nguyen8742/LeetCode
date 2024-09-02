adcea46b5c108487fc50e05f06eef7fe8981b9fb
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-02 18:24:59 -0500

    Time: 81 ms (54.65%), Space: 60.7 MB (31.40%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.js
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.js
@@ -5,12 +5,12 @@
  */
 var chalkReplacer = function(chalk, k) {
   const totalChalk = chalk.reduce((sum, pieces) => sum + pieces, 0);
-  k = k % totalChalk;
+  let target = k % totalChalk;
 
   for (let i = 0; i < chalk.length; i++) {
-    if (k < chalk[i]) {
+    if (target < chalk[i]) {
       return i;
     }
-    k -= chalk[i];
+    target -= chalk[i];
   }
 };
