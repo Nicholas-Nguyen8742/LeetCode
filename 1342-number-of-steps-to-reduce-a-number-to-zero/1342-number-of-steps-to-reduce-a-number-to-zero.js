375b02db66b844575893209107d34b9b02d14ce8
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-07 14:47:18 -0500

    Time: 42 ms (95.84%), Space: 49.1 MB (21.37%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 var numberOfSteps = function(num, steps = 0) {
-  if (num === 0) {
-    return steps;
-  }
-    
+  if (num === 0) return steps;
   return numberOfSteps(num % 2 === 0 ? (num / 2) : (num - 1), steps + 1);
 };
