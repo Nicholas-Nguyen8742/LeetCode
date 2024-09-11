05286ee0755788e49149138a216cae8fc68dda3f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-11 18:09:54 -0500

    Time: 57 ms (72.51%), Space: 49.2 MB (91.81%) - LeetHub

--- 3136-valid-word/3136-valid-word.js
+++ 3136-valid-word/3136-valid-word.js
@@ -3,19 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(word) {
-  if (word.length < 3) {
-    return false;
-  }
-
-  if (!(word.search(/[^a-zA-Z\d:]/i) < 0)) {
-    return false;
-  }
-
-  if (!(word.search(/^[^aeiou]+$/i) < 0)) {
-    return false;
-  }
-
-  if (!(word.search(/^[^bcdfghjklmnpqrstvwxyz]+$/i) < 0)) {
+  if (word.length < 3 || !(word.search(/[^a-zA-Z\d:]/i) < 0) || !(word.search(/^[^aeiou]+$/i) < 0) || !(word.search(/^[^bcdfghjklmnpqrstvwxyz]+$/i) < 0)) {
     return false;
   }
 
