57c355dbff745b563e54e21a81b4cfac3e8092d9
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-11 18:09:04 -0500

    Time: 59 ms (64.33%), Space: 49.4 MB (84.21%) - LeetHub

--- 3136-valid-word/3136-valid-word.js
+++ 3136-valid-word/3136-valid-word.js
@@ -11,8 +11,18 @@ var isValid = function(word) {
   if (word.length < 3) {
     return false;
   }
-    
-  const cases = [word.search(/[^a-zA-Z\d:]/i) < 0, word.search(/^[^aeiou]+$/i) < 0, word.search(/^[^bcdfghjklmnpqrstvwxyz]+$/i) < 0];
 
-  return everyTrue(cases);
+  if (!(word.search(/[^a-zA-Z\d:]/i) < 0)) {
+    return false;
+  }
+
+  if (!(word.search(/^[^aeiou]+$/i) < 0)) {
+    return false;
+  }
+
+  if (!(word.search(/^[^bcdfghjklmnpqrstvwxyz]+$/i) < 0)) {
+    return false;
+  }
+
+  return true;
 };
