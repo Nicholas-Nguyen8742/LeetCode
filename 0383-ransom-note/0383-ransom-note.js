805d266fe35d7c42deb67850cb273a1180432399
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-12 18:06:46 -0500

    Time: 75 ms (47.31%), Space: 55.9 MB (13.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -6,6 +6,9 @@
 var canConstruct = function(ransomNote, magazine) {
   const ransomArr = ransomNote.split('');
   for (let i = 0; i < magazine.length; i++) {
+    if (!ransomArr.includes(magazine[i])) {
+      continue;
+    }
     const foundIndex = ransomArr.findIndex((el) => el === magazine[i]);
     if (foundIndex === -1) {
       continue;
