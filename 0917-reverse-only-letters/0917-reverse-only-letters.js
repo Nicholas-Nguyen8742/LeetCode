b700f2d1fdfcb311ed57803b8a6e8f9fc84cd901
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-12 19:47:12 -0500

    Time: 63 ms (10.17%), Space: 48.8 MB (75.85%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.js
+++ 0917-reverse-only-letters/0917-reverse-only-letters.js
@@ -5,23 +5,14 @@
 var reverseOnlyLetters = function(s) {
   const isAlphabetic = /^[a-zA-Z]+$/;
   const letters = [];
-  
-  return s.split('').reduce((acc, curr, i) => {
+  const result = [];
+  s.split('').forEach((curr, i) => {
     if (isAlphabetic.test(curr)) {
       letters.unshift(curr);
-      acc.push(null);
+      result.push(null);
     } else {
-      acc.push(curr);
+      result.push(curr);
     }
-    if (s.length - 1 === i) {
-      return acc.map((el) => {
-        if (el == null) {
-          return letters.splice(0, 1)[0];
-        } else {
-          return el;
-        }
-      }).join('');
-    }
-    return acc;
-  }, []);
+  });
+  return result.map((el, i) => el === null ? letters.splice(0, 1)[0] : el).join('');
 };
