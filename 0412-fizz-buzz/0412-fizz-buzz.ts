73a29f2f4238f7eff50f62a500f5f17b36458017
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-15 12:54:58 -0500

    Time: 63 ms (61.85%), Space: 52.9 MB (47.49%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.ts
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -1,22 +1,11 @@
 function fizzBuzz(n: number): string[] {    
   const arr = [];
 
-  // Loop starting at 1 then going to target - (n)
   for (let i = 1; i <= n; i++) {
-    // Condition: None of above - Action: "<number>"
-    let indexString = `${i}`;
-
-    // Condition: Divisible by 3 - Action: Fizz
-    if (i % 3 === 0) {
-      indexString = "Fizz";
-    }
-
-    // Condition: Divisble by 5 - Action: Buzz
-    if (i % 5 === 0) {
-      indexString.includes("Fizz") ? indexString += "Buzz" : indexString = "Buzz";
-    }
+    const fizz = i % 3 === 0 ? "Fizz" : '';
+    const buzz = i % 5 === 0 ? "Buzz" : '';
     
-    arr.push(indexString);
+    arr.push((i % 3 === 0 || i % 5 === 0) ? fizz + buzz : `${i}`);
   }
 
   return arr;
