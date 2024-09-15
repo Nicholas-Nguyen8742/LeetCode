ba6cd033c730cd77bb89898de4ef3ede00473925
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-15 12:56:25 -0500

    Time: 67 ms (39.18%), Space: 52.3 MB (93.85%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.ts
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -2,10 +2,12 @@ function fizzBuzz(n: number): string[] {
   const arr = [];
 
   for (let i = 1; i <= n; i++) {
-    const fizz = i % 3 === 0 ? "Fizz" : '';
-    const buzz = i % 5 === 0 ? "Buzz" : '';
+    const fizzCondition = i % 3 === 0;
+    const buzzCondition = i % 5 === 0;
+    const fizz = fizzCondition ? "Fizz" : '';
+    const buzz = buzzCondition ? "Buzz" : '';
     
-    arr.push((i % 3 === 0 || i % 5 === 0) ? fizz + buzz : `${i}`);
+    arr.push((fizzCondition || buzzCondition) ? fizz + buzz : `${i}`);
   }
 
   return arr;
