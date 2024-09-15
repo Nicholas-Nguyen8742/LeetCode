eb1de07e6f930b453acf7eebaf25eb81d179ddb6
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-15 13:30:14 -0500

    Time: 23 ms (51.14%), Space: 12.1 MB (99.80%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -5,22 +5,22 @@ class Solution(object):
         :rtype: List[str]
         """
         list = []
-        
         i = 1
-
-        while i < n + 1:
+        while i <= n:
           fizzCondition = i % 3 == 0
           buzzCondition = i % 5 == 0
           
-          if fizzCondition == False and buzzCondition == False:
-              list.append(str(i))
-          elif fizzCondition and buzzCondition:
-              list.append("FizzBuzz")
-          elif buzzCondition:
-              list.append("Buzz")
+          if fizzCondition or buzzCondition:
+              listItem = ""
+              if fizzCondition: 
+                  listItem = "Fizz"
+
+              if buzzCondition:
+                  listItem = listItem + "Buzz"
+
+              list.append(listItem)              
           else: 
-              list.append("Fizz")
-              
+              list.append(str(i))
           i += 1
 
         return list
