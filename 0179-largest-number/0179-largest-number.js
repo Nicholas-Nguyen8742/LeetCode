0f555c83edcebe7a05db4251ffdcf740991eec1e
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-18 17:22:17 -0500

    Time: 56 ms (59.50%), Space: 50.6 MB (73.66%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+  const arrString = nums.map((num) => num.toString());
+  arrString.sort((a, b) => (b + a) - (a + b));
+  if (arrString[0] === '0') {
+    return '0';
+  }
+
+  return arrString.join('');
+};
