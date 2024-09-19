93a5df53bfd5b5c0e288131c40db75ea47d1682b
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-19 18:08:02 -0500

    Time: 76 ms (80.57%), Space: 58.1 MB (35.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -2,5 +2,10 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-  s.reverse();
+  const copy: string[] = [...s];
+  let curr: number = 0;
+  while (curr < s.length) {
+    s[curr] = copy[s.length - 1 - curr];
+    curr += 1;
+  }
 };
