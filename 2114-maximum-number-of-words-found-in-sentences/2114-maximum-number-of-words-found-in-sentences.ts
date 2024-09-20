29b37cc0054fab1d6e25d1f7ba5ed83205d0bb01
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 08:47:51 -0500

    Time: 63 ms (44.00%), Space: 52.2 MB (44.00%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.ts
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.ts
@@ -1,12 +1,6 @@
 function mostWordsFound(sentences: string[]): number {
-  let max = handleWordCount(sentences[0]);
-  for (let i = 1; i < sentences.length; i++) {
-    const currLength = handleWordCount(sentences[i]);
-    if (max < currLength) {
-      max = currLength;
-    }
-  }
-  return max;
+  const arrayOfCounts = sentences.map(handleWordCount);
+  return Math.max(...arrayOfCounts);
 };
 
 function handleWordCount(s: string): number {
