03215e91149ea0edff4c7c25999552977d13b846
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 08:34:07 -0500

    Time: 39 ms (23.47%), Space: 16.4 MB (87.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
         array_string = s.strip().split()
         return len(array_string[len(array_string) - 1])
