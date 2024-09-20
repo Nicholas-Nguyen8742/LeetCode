68623423ed7ea26368dac468f48169166529b946
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 08:29:36 -0500

    Time: 41 ms (35.45%), Space: 16.5 MB (92.00%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         for char in t:
-          if char not in s:
-            return char
-          else:
+          if char in s:
             s = s.replace(char, '', 1)
+          else:
+            return char
