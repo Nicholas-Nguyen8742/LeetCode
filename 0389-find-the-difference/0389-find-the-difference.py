f6026560f67a716bb6188c4435b76dffb2857269
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 08:29:56 -0500

    Time: 26 ms (18.40%), Space: 11.5 MB (92.13%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,12 @@
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
         for char in t:
-          if char in s:
-            s = s.replace(char, '', 1)
-          else:
-            return char
+            if char in s:
+                s = s.replace(char, '', 1)
+            else:
+                return char
