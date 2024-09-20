204fb5210d0c49d2af90ba8185f6ecb41bfd5d33
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-19 19:17:53 -0500

    Time: 180 ms (6.97%), Space: 20.9 MB (30.28%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def reverseString(self, s):
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
         """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
+        Do not return anything, modify s in-place instead.
         """
         copy = list(s)
         i = 0
         while i < len(s):
             s[i] = copy[len(s) - i - 1]
             i += 1
-
         
