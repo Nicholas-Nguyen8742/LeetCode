131ff53200730cdabf647a29d382489b6a073381
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 19:24:42 -0500

    Time: 78 ms (80.97%), Space: 55 MB (19.75%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.ts
@@ -0,0 +1,42 @@
+function myAtoi(s: string): number {
+  // 1) Ignore any leading whitespaces;
+  const cleanString = s.trim();
+  if (!cleanString.length) {
+    return 0;
+  }
+  
+  // 2) "-" = negative
+  let prefix = "";
+  let index = 0;
+  
+  if (["-", "+"].includes(cleanString[0])) {
+    if (cleanString[0] === "-") {
+      prefix = "-";
+     }
+    index = 1;
+  }
+  
+  if (cleanString[index] == null) {
+    return 0;
+  }
+
+  let result = "";
+
+  function getResult(result) {
+    return Math.max(Math.min(result, Math.pow(2, 31) - 1), -Math.pow(2, 31))
+  }
+  
+  for (let i = index; i < cleanString.length; i++) {
+    if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(cleanString[i])) {
+      result =  result + cleanString[i];
+    } else {
+      if (result === "") {
+        return 0;
+      }
+      return getResult(parseInt(prefix + result, 10));
+    }
+  }
+
+  // Return if no other conditions met
+  return getResult(parseInt(prefix + result, 10));
+};
