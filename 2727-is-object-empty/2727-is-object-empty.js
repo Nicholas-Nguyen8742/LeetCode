523c0e56e84155e9d1352b1d91dc1fe1c8ee0fb0
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 23:50:51 -0500

    Time: 52 ms (54.85%), Space: 49 MB (81.92%) - LeetHub

--- 2727-is-object-empty/2727-is-object-empty.js
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -6,12 +6,6 @@ var isEmpty = function(obj) {
   if (Array.isArray(obj)) {
     return obj.length === 0;
   }
-  
-  let isObject = true;
-  for (const key in obj) {
-    if (key != null) {
-      return false;
-    }
-  }
-  return isObject;
+
+  return Object.keys(obj).length === 0;
 };
