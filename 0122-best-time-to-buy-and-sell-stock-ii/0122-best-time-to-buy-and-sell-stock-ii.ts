6aa79024c84ec005ab5e612a3d08d2d9895bd10e
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-20 23:17:44 -0500

    Time: 59 ms (64.54%), Space: 52.5 MB (7.88%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -1,10 +1,10 @@
 function maxProfit(prices: number[]): number {
-  let [curr, profit] = [-Infinity, 0];
-  prices.forEach((price) => {
-    const [prev, prevProfit] = [curr, profit];
-    curr = Math.max(prev, prevProfit - price);
-    profit = Math.max(prevProfit, prev + price);
-  })
-
-  return profit;
+  const result = prices.reduce((acc, price, i) => {
+    const [prev, prevProfit] = [acc[0], acc[1]];
+    acc[0] = Math.max(prev, prevProfit - price);
+    acc[1] = Math.max(prevProfit, prev + price);
+    return acc;
+  }, [-Infinity, 0]);
+  
+  return result[1];
 };
