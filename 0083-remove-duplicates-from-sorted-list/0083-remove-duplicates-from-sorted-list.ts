69356c55bb9c119660ac07e409da1621b1a5317f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-22 17:06:59 -0500

    Time: 63 ms (85.56%), Space: 53.3 MB (39.29%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let temp = head;
+  while (temp) {
+    if (temp.next !== null && temp.val == temp.next.val) {
+      temp.next = temp.next.next;
+    } else {
+      temp = temp.next;
+    }
+  }
+
+  return head;
+};
