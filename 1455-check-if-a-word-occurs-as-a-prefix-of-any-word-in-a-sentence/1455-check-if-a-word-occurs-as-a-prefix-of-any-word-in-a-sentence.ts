254a9753dcae2978a4226aa8230f900b50662028
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-25 17:08:48 -0500

    Time: 49 ms (78.26%), Space: 50.6 MB (17.39%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.ts
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.ts
@@ -1,11 +1,11 @@
 function isPrefixOfWord(sentence: string, searchWord: string): number {
   const arrayOfWords = sentence.split(' ');
   let i = 0;
-  while (i < arrayOfWords.length) {
+  do {
     if (arrayOfWords[i].startsWith(searchWord)) {
       return i + 1;
     }
     i++;
-  }
+  } while (i < arrayOfWords.length)
   return -1;
 };
