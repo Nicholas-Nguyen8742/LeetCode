3eaf326020b3da794d974085432434aec9aa289c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-26 13:11:45 -0500

    Time: 148 ms (62.74%), Space: 60.1 MB (60.78%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.ts
+++ 0729-my-calendar-i/0729-my-calendar-i.ts
@@ -1,22 +1,25 @@
 class MyCalendar {
-  bookings: [number, number][];
+  starts: number[];
+  ends: number[];
+
   constructor() {
-    this.bookings = [];
+    this.starts = [];
+    this.ends = [];
   }
 
   book(start: number, end: number): boolean {
-    for (let i = 0; i < this.bookings.length; i++) {
-      const [s, e] = this.bookings[i];
-      if (!this.bookings[i]) {
+    for (let i = 0; i < this.starts.length; i++) {
+      if (this.starts[i] == null || this.ends[i] == null) {
         break;
       }
 
-      if (s < end && start < e) {
+      if (this.starts[i] < end && start < this.ends[i]) {
         return false;
       }
     }
 
-    this.bookings.push([start, end]);
+    this.starts.push(start);
+    this.ends.push(end);
     return true;
   }
 }
