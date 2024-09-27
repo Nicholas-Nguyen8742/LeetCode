e0afaee50f14aac2247d9547fdaa7753a1c9bd6c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-27 08:42:26 -0500

    Time: 78 ms (11.84%), Space: 52.6 MB (44.20%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -10,20 +10,22 @@
  * }
  */
 
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-  let list = new ListNode(0);
-  let curr = list;
-  let temp1 = list1;
-  let temp2 = list2;
-  while (temp1 || temp2) {
-    if (temp1 === null || (temp2 !== null && temp2.val < temp1.val)) {
-      curr.next = temp2;
-      temp2 = temp2.next;
-    } else {
-      curr.next = temp1;
-      temp1 = temp1.next;
-    }
-    curr = curr.next;
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null, list: ListNode | null = new ListNode(0), curr: ListNode | null = null): ListNode | null {
+  if (curr == null) {
+    curr = list;
   }
-  return list.next;
+
+  if (list1 == null && list2 == null && list != null) {
+    return list.next;
+  }
+
+  if (list1 === null || (list2 !== null && list2.val < list1.val)) {
+    curr.next = list2;
+    list2 = list2.next;
+  } else {
+    curr.next = list1;
+    list1 = list1.next;
+  }
+  curr = curr.next;
+  return mergeTwoLists(list1, list2, list, curr);
 };
