f235d4dffe7d586690e0cce52c55c34786aa8dbe
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-27 15:48:40 -0500

    Time: 190 ms (26.00%), Space: 59.9 MB (36.00%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.js
+++ 0731-my-calendar-ii/0731-my-calendar-ii.js
@@ -1,47 +1,48 @@
-
-var MyCalendarTwo = function() {
-  this.bookings = [];
-  this.overlappedBookings = []
-};
-
-/** 
- * @param {number} start 
- * @param {number} end
- * @return {boolean}
- */
-MyCalendarTwo.prototype.book = function(start, end) {
-  for (let i = 0; i < this.overlappedBookings.length; i++) {
-    const [s, e] = this.overlappedBookings[i];
-    if (!this.overlappedBookings[i]) {
-      break;
+class MyCalendarTwo {
+  constructor() {
+    this.bookings = [];
+    this.overlappedBookings = [];
+  }
+  /** 
+   * @param {number} start 
+   * @param {number} end
+   * @return {boolean}
+   */  
+  book(start, end) {
+    for (let i = 0; i < this.overlappedBookings.length; i++) {
+      const [s, e] = this.overlappedBookings[i];
+      if (!this.overlappedBookings[i]) {
+        break;
+      }
+      if (this.areOverlapping(start, end, s, e)) {
+        return false;
+      }
     }
-    if (this.areOverlapping(start, end, s, e)) {
-      return false;
+
+    for (let i = 0; i < this.bookings.length; i++) {
+      const [s, e] = this.bookings[i];
+      if (!this.bookings[i]) {
+        break;
+      }
+
+      if (this.areOverlapping(s, e, start, end)) {
+        this.overlappedBookings.push(this.getOverlapped(s, e, start, end));
+      }
     }
+
+    this.bookings.push([start, end]);
+    return true;
+  }
+  
+  areOverlapping(start1, end1, start2, end2) {
+    return Math.max(start1, start2) < Math.min(end1, end2);
   }
 
-  for (let i = 0; i < this.bookings.length; i++) {
-    const [s, e] = this.bookings[i];
-    if (!this.bookings[i]) {
-      break;
-    }
-
-    if (this.areOverlapping(s, e, start, end)) {
-      this.overlappedBookings.push(this.getOverlapped(s, e, start, end));
-    }
+  getOverlapped(start1, end1, start2, end2) {
+    return [Math.max(start1, start2), Math.min(end1, end2)];
   }
+}
 
-  this.bookings.push([start, end]);
-  return true;
-};
-
-MyCalendarTwo.prototype.areOverlapping = function(start1, end1, start2, end2) {
-  return Math.max(start1, start2) < Math.min(end1, end2);
-};
-
-MyCalendarTwo.prototype.getOverlapped = function(start1, end1, start2, end2) {
-  return [Math.max(start1, start2), Math.min(end1, end2)];
-};
 
 /** 
  * Your MyCalendarTwo object will be instantiated and called as such:
