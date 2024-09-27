897281dfbf3a3495adae3380433ad817351cca72
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-27 17:45:27 -0500

    Time: 68 ms (18.50%), Space: 52 MB (12.26%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  let temp = head;
+  let before = new ListNode(0);
+  let dummyHead = before;
+
+  const map = new Map();
+  if (!temp || temp.next == null) {
+    return head;
+  }
+
+  while (temp) {
+    if (map.has(temp.val)) {
+      map.set(temp.val, true);
+    } else {
+      map.set(temp.val, false);
+    }
+    temp = temp.next;
+  }
+
+  temp = head;
+  let prev = null;
+  
+  map.forEach((val, key) => {
+    if (val === false) {
+      dummyHead.next = new ListNode(key);
+      dummyHead = dummyHead.next;
+    }
+  });
+
+  return before.next;
+};
