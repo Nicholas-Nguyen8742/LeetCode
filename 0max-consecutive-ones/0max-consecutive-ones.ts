9270e4e781d7e38bd60d8654f3401896cf255d81
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-27 16:16:20 -0500

    Time: 59 ms, Memory: 55.2 MB - LeetHub

---
+++ 0max-consecutive-ones/0max-consecutive-ones.ts
@@ -0,0 +1,14 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let count = 0;
+  let currCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      currCount++;
+      count = Math.max(count, currCount);
+    } else {
+      currCount = 0;
+    }
+  }
+
+  return count;
+};
