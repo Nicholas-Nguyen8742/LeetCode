8a92cce3966dcd62ec0b48a1e09f033c0514b9e2
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-27 16:51:15 -0500

    Time: 83 ms (85.99%), Space: 57.2 MB (60.58%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+  if (!lists || !lists.length) {
+    return null;
+  }
+
+  let head = new ListNode(0);
+  let valuesArray = [];
+
+  for (let i = 0; i < lists.length; i++) {
+    let currList = lists[i];
+    while (currList) {
+      valuesArray.push(currList.val);
+      currList = currList.next;
+    }
+  }
+  
+  let curr = head;
+  valuesArray
+    .sort((a, b) => a - b)
+    .forEach((num) => {
+      let temp = new ListNode(num);
+      curr.next = temp;
+      curr = curr.next;
+    });
+  
+  return head.next;
+};
