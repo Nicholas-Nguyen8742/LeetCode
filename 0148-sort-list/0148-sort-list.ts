2f2383b12267cf035e39440af94570c7c03f864c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-09-29 18:59:01 -0500

    Time: 178 ms (37.69%), Space: 73.1 MB (59.23%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function sortList(head: ListNode | null): ListNode | null {
+  const result = [];
+  let temp = head;
+  
+  while (temp) {
+    if (temp.val != null) {
+      result.push(temp.val);
+    }
+    temp = temp.next;
+  }
+  
+  result.sort((a, b) => a - b);
+
+  let list = new ListNode(0);
+  let tempList = list;
+  result.forEach((num) => {
+    const node = new ListNode(num);
+    tempList.next = node;
+    tempList = tempList.next;
+  });
+  return list.next;
+};
