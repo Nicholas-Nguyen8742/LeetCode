6f85b18df9512d9e480f9d93f5389e60e1a690cd
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-02 18:09:42 -0500

    Time: 141 ms (52.28%), Space: 69.9 MB (78.40%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -9,12 +9,16 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head, result = []) {
-  if (head != null) {
-    result.push(head.val);
-    head = head.next;
-    return isPalindrome(head, result);
-  }
-
-  return result.toReversed().join('') === result.join('');
+var isPalindrome = function(head) {
+  let slow = head, fast = head, prev, temp
+  while (fast && fast.next)
+      slow = slow.next, fast = fast.next.next
+  prev = slow, slow = slow.next, prev.next = null
+  while (slow)
+      temp = slow.next, slow.next = prev, prev = slow, slow = temp
+  fast = head, slow = prev
+  while (slow)
+      if (fast.val !== slow.val) return false
+      else fast = fast.next, slow = slow.next
+  return true
 };
