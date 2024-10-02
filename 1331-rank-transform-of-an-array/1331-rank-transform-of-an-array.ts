17a8b9800960156838255f741caaafe9b3b640e7
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-02 17:23:37 -0500

    Time: 218 ms (21.43%), Space: 77.2 MB (42.86%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
@@ -1,17 +1,17 @@
 function arrayRankTransform(arr: number[]): number[] {
   const sorted = [...arr].sort((a, b) => a - b);
 
-  const map = new Map();
+  const map = Object.create(null);
 
   let rank = 1;
   let curr = 0;
   while (curr < sorted.length) {
-    if (!map.has(sorted[curr])) {
-      map.set(sorted[curr], rank);
+    if (!(sorted[curr] in map)) {
+      map[sorted[curr]] = rank;
       rank++;
     }
     curr++;
   }
 
-  return arr.map((el) => map.get(el));
+  return arr.map((el) => map[el]);
 };
