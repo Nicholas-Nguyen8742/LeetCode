b7e735211e15eb8705bec8e3c61e54402edd50d0
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-04 11:01:25 -0500

    Time: 1121 ms (5.26%), Space: 60.5 MB (53.95%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.js
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.js
@@ -5,22 +5,19 @@
 var dividePlayers = function(skill) {
   skill.sort((a, b) => a - b);
 
-  let result = [];
-
   let teamChemistry = 0;
-  let totalSkill = skill[0] + skill[skill.length - 1];
+  const totalSkill = skill[0] + skill[skill.length - 1];
 
   while (skill.length) {
     const firstEl = skill.shift();
     const lastEl = skill.pop();
-    
+
     if (totalSkill !== (firstEl + lastEl)) {
       return -1;
     }
-    
+
     teamChemistry += (firstEl * lastEl)
   }
-  
-  
+
   return teamChemistry;
 };
