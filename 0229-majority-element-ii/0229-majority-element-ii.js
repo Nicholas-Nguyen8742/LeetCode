46cd427e4da120a10a2ac0275e71275e03c4cb80
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-04 14:32:00 -0500

    Time: 55 ms (86.28%), Space: 52 MB (50.27%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -4,16 +4,16 @@
  */
 var majorityElement = function(nums) {
   if (nums.length < 3) {
-    const set = new Set(nums);
-    return Array.from(set);
+    return Array.from(new Set(nums));
   }
+
   const hash = new Map();
   let result = [];
 
   for (let i = 0; i < nums.length; i++) {
-    const element = hash.get(nums[i]);
-    if (element) {
-      const sum = element + 1;
+    const currCount = hash.get(nums[i]);
+    if (currCount) {
+      const sum = currCount + 1;
       if (sum > (nums.length / 3) && !result.includes(nums[i])) {
         result.push(nums[i]);
       }
@@ -22,6 +22,6 @@ var majorityElement = function(nums) {
       hash.set(nums[i], 1);
     }
   }
-  
+
   return result;
 };
