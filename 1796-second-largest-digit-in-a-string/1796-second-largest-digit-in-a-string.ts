8f4689ca5b3bd26936e1dc4b0e88de045e2f1d05
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-05 14:49:18 -0500

    Time: 72 ms (13.04%), Space: 51.7 MB (69.57%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.ts
@@ -0,0 +1,17 @@
+function secondHighest(s: string): number {
+  const cleanString = s.replaceAll(/[^\d]/g, '');
+  const stringNumberArray = cleanString.split('');
+  if (stringNumberArray.length === 0) {
+    return -1;
+  }
+  
+  const numberArray = stringNumberArray.map((str) => parseInt(str));
+  const set = new Set(numberArray);
+  if (set.size === 1) {
+    return -1;
+  }
+
+  let max = Math.max(...Array.from(set));
+  set.delete(max);
+  return Math.max(...Array.from(set));
+};
