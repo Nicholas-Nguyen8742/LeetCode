2f85ceef57fe27bf89d0bb2898813c3bb8e5169d
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-05 22:18:01 -0500

    Time: 52 ms (62.85%), Space: 49.1 MB (50.88%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+  let max = Math.max(...nums);
+  let maxIndex;
+  for (let i = 0; i < nums.length; i++) {
+    if (max === nums[i]) {
+      maxIndex = i;
+      continue;
+    }
+
+    if (2 * (nums[i]) <= max) {
+      continue;
+    }
+    return -1;
+  }
+  
+  return maxIndex;
+};
