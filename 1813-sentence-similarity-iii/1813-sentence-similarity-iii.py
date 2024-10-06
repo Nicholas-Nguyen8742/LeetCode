3f9e0d03ef4d48adfdee5d4a8b7b0faf4d2e2fca
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-05 22:11:02 -0500

    Time: 4 ms (100.00%), Space: 11.7 MB (23.08%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -1,14 +1,20 @@
-class Solution:
-    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+class Solution(object):
+    def areSentencesSimilar(self, sentence1, sentence2):
+        """
+        :type sentence1: str
+        :type sentence2: str
+        :rtype: bool
+        """
         s1 = list(sentence1.split())
         s2 = list(sentence2.split())
         
         while s1 and s2 and s1[0] == s2[0]:
-          s1.pop(0)
-          s2.pop(0)
+            s1.pop(0)
+            s2.pop(0)
           
         while s1 and s2 and s1[-1] == s2[-1]:
-          s1.pop(-1)
-          s2.pop(-1)
+            s1.pop(-1)
+            s2.pop(-1)
           
         return not s1 or not s2
+        
