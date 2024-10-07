edef0ca10e0bdcf1f680023b876261f16eb1c592
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-07 15:56:15 -0500

    Time: 92 ms (62.90%), Space: 54.8 MB (80.65%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.ts
@@ -1,7 +1,8 @@
 function minLength(s: string): number {
   const stack = [];
   for (const char of s) {
-    if (char == 'B' && stack[stack.length - 1] === 'A' || char == 'D' && stack[stack.length - 1] === 'C') {
+    const prev = stack[stack.length - 1];
+    if (char == 'B' && prev === 'A' || char == 'D' && prev === 'C') {
       stack.pop();
     } else {
       stack.push(char);
