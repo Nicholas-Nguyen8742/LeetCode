e0e3012b4294c3a57b95b07199016520798c5909
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-07 16:09:32 -0500

    Time: 46 ms (83.27%), Space: 48.9 MB (36.45%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+  const arr = s.split(" ");
+  if (pattern.length !== arr.length) {
+    return false;
+  }
+
+  const map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (!map.has(pattern[i]) && !map.has(`value-${arr[i]}`)) {
+      map.set(pattern[i], arr[i]);
+      map.set(`value-${arr[i]}`, pattern[i]);
+    }
+
+    if (map.get(pattern[i]) !== arr[i] && pattern[i] !== map.get(`value-${arr[i]}`)) {
+      return false;
+    }
+  }
+
+  return true;
+};
