9148fbd0e38a114fc66110c2187416f1c11d5a73
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-07 15:50:13 -0500

    Time: 78 ms (95.16%), Space: 56.4 MB (45.16%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
@@ -1,8 +1,4 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var minLength = function(s) {
+function minLength(s: string): number {
   while (s.includes('AB') || s.includes('CD')) {
     s = s.replaceAll('CD', '');
     s = s.replaceAll('AB', '');
