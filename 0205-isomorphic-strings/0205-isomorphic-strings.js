7850ac28fdc0ba2a09924129d404b38d613c3fe8
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-09 18:23:34 -0500

    Time: 52 ms (93.35%), Space: 50.2 MB (84.99%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -10,8 +10,7 @@ var isIsomorphic = function(s, t) {
 
   const map = new Map();
   const compareMap = new Map();
-  
-  
+
   for (let i = 0; i < s.length; i++) {
     const sVal = map.get(s[i]);
     const tVal = compareMap.get(t[i]);
