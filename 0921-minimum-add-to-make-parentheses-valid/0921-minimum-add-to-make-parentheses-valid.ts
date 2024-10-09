adcfd8b9d1c289d73194be8506c06fdf9297a663
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-09 17:09:58 -0500

    Time: 67 ms (20.00%), Space: 52.1 MB (20.00%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.ts
@@ -0,0 +1,22 @@
+function minAddToMakeValid(s: string): number {
+  if (!s.length) {
+    return 0; 
+  }
+
+  let open = 0;
+  let count = 0;
+  let i = 0;
+  while (i < s.length) {   
+    if (s[i] === '(') {
+      open++;
+    } else {
+      if (open > 0) {
+        open--;
+      } else {
+        count++;
+      }
+    }
+    i++;
+  }
+  return count + open;
+};
