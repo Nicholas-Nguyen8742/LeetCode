a68e963f9a0191031198822c22a418ed0a409f75
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-09 19:25:40 -0500

    Time: 92 ms (66.99%), Space: 57.7 MB (43.97%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.ts
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -2,7 +2,7 @@ function convert(s: string, numRows: number): string {
   if (numRows === 1 || s.length < numRows) {
     return s;
   }
-
+  
   const result = Array.from({ length: numRows }, () => []);
   let rowIndex = 0;
   let reverse = false;
@@ -10,8 +10,19 @@ function convert(s: string, numRows: number): string {
   for (let i = 0; i < s.length; i++) {
     result[rowIndex].push(s[i]);
     reverse ? rowIndex-- : rowIndex++;
-    if (rowIndex === numRows - 1 || rowIndex === 0) reverse = !reverse;
+    if (rowIndex === numRows - 1 || rowIndex === 0) {
+      reverse = !reverse;
+    }
   }
 
-  return result.flat().join('');
+  let resultString = '';
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      if (typeof result[i][j] === 'string') {
+        resultString += result[i][j];
+      }
+    }
+  }
+
+  return resultString;
 };
