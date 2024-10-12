94bdef23847cb24cc989e2e1bae02215300081bf
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-12 00:01:40 -0500

    Time: 58 ms (69.72%), Space: 50.9 MB (96.79%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.ts
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.ts
@@ -1,10 +1,6 @@
 function kthFactor(n: number, k: number): number {
   const arr = [];
   for (let i = 1; i <= n + 1; i++) {
-    // if (arr.length === k) {
-    //   return arr[n - 1];
-    // }
-
     if (n % i === 0) {
       if (arr.length === k - 1) {
           return i;
