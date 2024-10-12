8dce1899a04596484ce4694a20368731a2caec30
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-12 00:24:34 -0500

    Time: 65 ms (73.85%), Space: 51.6 MB (90.77%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.ts
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.ts
@@ -1,10 +1,12 @@
 function numberOfPairs(nums1: number[], nums2: number[], k: number): number {
   let count = 0;
   for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0; j < nums2.length; j++) {
+    let j = 0;
+    while (j < nums2.length) {
       if (nums1[i] % (nums2[j] * k) === 0) {
         count++;
       }
+      j++;
     }
   }
   return count;
