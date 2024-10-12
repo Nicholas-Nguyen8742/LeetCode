8cb8eb49a63aa17999bdeda5bd2219c0a9fac3fc
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-12 02:59:10 -0500

    Time: 68 ms (53.09%), Space: 52.4 MB (53.09%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.ts
@@ -0,0 +1,25 @@
+function maxFrequencyElements(nums: number[]): number {
+  let map = {};
+  let max = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const currCount = map[nums[i]];
+    if (currCount == null) {
+      map[nums[i]] = 1;
+      max = Math.max(max, 1); 
+    } else {
+      const newValue = currCount + 1;
+      map[nums[i]] = newValue;
+      max = Math.max(max, newValue);
+    }
+  }
+  
+  let multiplier = 0;
+  for (const [key, value] of Object.entries(map)) {
+    if (value === max) {
+      multiplier++;
+    }
+  }
+
+  return multiplier * max;
+};
