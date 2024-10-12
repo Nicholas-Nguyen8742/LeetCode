98ee69d29045913989c087214fb8c3dc65632169
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-12 02:46:33 -0500

    Time: 49 ms (99.39%), Space: 50.8 MB (92.07%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -6,11 +6,12 @@ var numberOfSpecialChars = function(word) {
   const uniqueWordArr = Array.from(new Set(word.split('')));
   const result = [];
   for (let i = 0; i < uniqueWordArr.length; i++) {
-    if (uniqueWordArr[i].toLowerCase() === uniqueWordArr[i]) {
+    const lowercaseCurr = uniqueWordArr[i].toLowerCase();
+    if (lowercaseCurr === uniqueWordArr[i]) {
       continue;
     }
 
-    if (uniqueWordArr.includes(uniqueWordArr[i].toLowerCase())) {
+    if (uniqueWordArr.includes(lowercaseCurr)) {
       result.push(i);
     }
   }
