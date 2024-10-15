21d9eb3e18b0f63f44e98cbb228b0e3f875b3b52
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-15 16:37:20 -0500

    Time: 69 ms (93.33%), Space: 54.6 MB (80.00%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.ts
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.ts
@@ -1,11 +1,12 @@
 function minimumSteps(s: string): number {
-  let whiteBallPosition = 0;
+  let blackBallTotal = 0;
   let total = 0;
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] === "0") {
-      total += i - whiteBallPosition;
-      whiteBallPosition += 1;
+      total += blackBallTotal;
+    } else {
+      blackBallTotal++;
     }
   }
 
