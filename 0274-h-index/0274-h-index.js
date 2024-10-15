c1fdf126c03b7a6f05465a6d8db51886cb47ad2b
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-15 18:14:29 -0500

    Time: 42 ms (97.28%), Space: 49.7 MB (24.39%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+  const n = citations.length;
+
+  citations.sort((a, b) => a - b);
+
+  for (let i = 0; i < n; i++) {
+    if (citations[i] >= n - i) {
+      return n - i;
+    }
+  }
+
+  return 0;
+};
