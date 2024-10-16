1abf419b7b056f991b4e62946a2bf9f106b65ede
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-15 20:24:42 -0500

    Time: 55 ms (87.88%), Space: 51.1 MB (100.00%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.ts
@@ -0,0 +1,27 @@
+function longestDiverseString(a: number, b: number, c: number): string {
+  let currA = 0, currB = 0, currC = 0;
+  let result = '';
+  const totalCount = a + b + c;
+  for (let i = 0; i < totalCount; i++) {
+    if ((a >= b && a >= c && currA != 2) || (a > 0 && (currB == 2 || currC == 2))) {
+      result += 'a';
+      a -= 1;
+      currA++;
+      currB = 0;
+      currC = 0;
+    } else if ((b >= a && b >= c && currB != 2) || (b > 0 && (currC == 2 || currA == 2))) {
+      result += 'b';
+      b--;
+      currB += 1;
+      currA = 0;
+      currC = 0;
+    } else if ((c >= a && c >= b && currC != 2) || (c > 0 && (currA == 2 || currB == 2))) {
+      result += "c"
+      c -= 1
+      currC += 1
+      currA = 0
+      currB = 0
+    }
+  }
+  return result;
+};
