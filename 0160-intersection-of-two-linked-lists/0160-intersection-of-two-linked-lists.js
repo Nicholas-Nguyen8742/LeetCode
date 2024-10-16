16f330c9f0da7fc6db9d09f8160238c199591394
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-16 12:56:39 -0500

    Time: 72 ms (85.93%), Space: 58.3 MB (69.21%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  let tempA = headA, tempB = headB;
+  while (tempA !== tempB) {
+    tempA = !tempA ? headB : tempA.next;
+    tempB = !tempB ? headA : tempB.next;
+  }
+  return tempA;
+};
