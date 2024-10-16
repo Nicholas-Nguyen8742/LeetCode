4fb48c15138bcd9d050078a8f42eab2bec7560b1
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-16 17:17:43 -0500

    Time: 81 ms (68.27%), Space: 58.8 MB (27.86%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -10,7 +10,7 @@ var findRestaurant = function(list1, list2) {
   for (let i = 0; i < list1.length; i++) {
     map.set(list1[i], [i, null]);
   }
-  
+
   for (let j = 0; j < list2.length; j++) {
     const hasValue = map.has(list2[j]);
     if (hasValue) {
@@ -19,16 +19,16 @@ var findRestaurant = function(list1, list2) {
       min = Math.min(value[0] + j, min);
     }
   }
-  
-  map.forEach((value, key, map) => {
-    if (value[1] == null) {
+
+  map.forEach(([val1, val2], key, map) => {
+    if (val2 == null) {
       return;
     }
-    const indexSum = value[0] + value[1];
+    const indexSum = val1 + val2;
     if (indexSum === min) {
       result.push(key);
     }
   });
-  
+
   return result;
 };
