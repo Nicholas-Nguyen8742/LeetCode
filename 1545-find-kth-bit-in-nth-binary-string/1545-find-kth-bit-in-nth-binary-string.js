251312d3ab07747689bf6455d10c779876901039
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-19 14:44:16 -0500

    Time: 0 ms (100.00%), Space: 48.9 MB (61.82%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
@@ -4,15 +4,21 @@
  * @return {character}
  */
 var findKthBit = function(n, k) {
-  if (n === 1) return '0';
-    
+  if (n === 1) {
+    return '0';
+  }
+
   const length = (1 << n) - 1;
 
   const mid = Math.floor(length / 2) + 1;
 
-  if (k === mid) return '1';
+  if (k === mid) {
+    return '1';
+  }
 
-  if (k < mid) return findKthBit(n - 1, k);
+  if (k < mid) {
+    return findKthBit(n - 1, k);
+  }
 
   return findKthBit(n - 1, length - k + 1) === '0' ? '1' : '0';
 };
