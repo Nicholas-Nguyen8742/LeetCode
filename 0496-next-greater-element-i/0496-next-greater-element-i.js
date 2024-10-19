51aa41631f0d8d75e2cdb012e9daeb86ff933f2c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-19 14:28:01 -0500

    Time: 23 ms (100.00%), Space: 56.8 MB (5.21%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+  const result = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    let foundIndex = nums2.findIndex((el) => el === nums1[i]);
+    if (foundIndex === -1) {
+      result.push(foundIndex);
+      continue;
+    }
+    
+    const newN2 = nums2.slice(foundIndex + 1);
+    let newFoundIndex = newN2.findIndex((el) => el > nums1[i]);
+    if (newFoundIndex === -1) {
+      result.push(-1);
+      continue;
+    }
+
+    result.push(newN2[newFoundIndex]);
+  }
+  
+  return result;
+};
