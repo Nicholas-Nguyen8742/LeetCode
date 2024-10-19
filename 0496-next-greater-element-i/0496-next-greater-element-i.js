772422b1fabf5e9b7ae918458f918e7d9662f00e
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-19 14:32:26 -0500

    Time: 20 ms (100.00%), Space: 67.5 MB (5.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,25 +3,27 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(nums1, nums2) {
-  const result = [];
-
-  for (let i = 0; i < nums1.length; i++) {
-    let foundIndex = nums2.findIndex((el) => el === nums1[i]);
-    if (foundIndex === -1) {
-      result.push(foundIndex);
-      continue;
-    }
-    
-    const newN2 = nums2.slice(foundIndex + 1);
-    foundIndex = newN2.findIndex((el) => el > nums1[i]);
-    if (foundIndex === -1) {
-      result.push(-1);
-      continue;
-    }
-
-    result.push(newN2[foundIndex]);
+var nextGreaterElement = function(nums1, nums2, result = []) {
+  if (nums1.length === 0) {
+    return result;
   }
+
+  const curr = nums1.pop();
   
-  return result;
+  let foundIndex = nums2.findIndex((el) => el === curr);
+  if (foundIndex === -1) {
+    result.unshift(foundIndex);
+    return nextGreaterElement(nums1, nums2, result);
+  }
+
+  const newN2 = nums2.slice(foundIndex + 1);
+  foundIndex = newN2.findIndex((el) => el > curr);
+  if (foundIndex === -1) {
+    result.unshift(-1);
+    return nextGreaterElement(nums1, nums2, result);
+  }
+
+  result.unshift(newN2[foundIndex]);
+
+  return nextGreaterElement(nums1, nums2, result);
 };
