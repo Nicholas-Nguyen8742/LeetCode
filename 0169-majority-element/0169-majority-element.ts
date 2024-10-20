17c8fe2eedc084f679c69b378ae588929953521f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-20 02:02:07 -0500

    Time: 9 ms (99.23%), Space: 55.1 MB (16.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,6 +1,27 @@
 function majorityElement(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+  let curr = nums[0];
+  let majority = 0;
+
   const map = new Map();
-  nums.forEach((el) => map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1));
-  const keys = [...map.keys()];
-  return keys[[...map.values()].indexOf(Math.max(...map.values()))];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (curr === nums[i]) {
+      majority++;
+    } else {
+      majority = 0;
+      curr = nums[i];
+    }
+    map.set(curr, majority);
+  }
+
+  let k = 0;
+  let kElement = nums[0];
+  const values = map.forEach((val, key) => {
+    k = Math.max(k, val);
+    if (val === k) {
+      kElement = key;
+    }
+  });
+  return kElement;
 };
