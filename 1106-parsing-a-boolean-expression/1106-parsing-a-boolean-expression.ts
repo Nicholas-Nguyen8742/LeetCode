5f032d62f5929d7c1ba0cd591ce0897c0915b473
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-19 22:19:31 -0500

    Time: 8 ms (100.00%), Space: 57 MB (50.00%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.ts
@@ -0,0 +1,34 @@
+function parseBoolExpr(expression: string): boolean {
+  const stack = [];
+  
+  for (const currChar of expression) {
+    if (currChar !== ')' && currChar !== ',') {
+      stack.push(currChar);
+    } else if (currChar === ')') {
+      let exp = [];
+
+      while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+        let t = stack.pop();
+        exp.push(t === 't');
+      }
+
+      stack.pop();
+
+      if (stack.length > 0) {
+        const t = stack.pop();
+        let v = exp[0];
+
+        if (t === '&') {
+          for (let b of exp) v = v && b;
+        } else if (t === '|') {
+          for (let b of exp) v = v || b;
+        } else {
+          v = !v;
+        }
+
+        stack.push(v ? 't' : 'f');
+      }
+    }
+  }
+  return stack[stack.length - 1] == 't';
+};
