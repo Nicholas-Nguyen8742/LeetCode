cbdea228b7d085ebbe89172cb00f04979756f1fe
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-26 12:56:45 -0500

    Time: 51 ms (93.38%), Space: 64.3 MB (31.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,15 +1,15 @@
 function groupAnagrams(strs: string[]): string[][] {
-  const map = new Map();
-  
+  const map = {};
+
   for (let i = 0; i < strs.length; i++) {
     const currWord = strs[i];
     const transformedWord = currWord.split('').sort().join('');
-    if (map.has(transformedWord)) {
-      map.set(transformedWord, [...map.get(transformedWord), currWord])
+    if (transformedWord in map) {
+      map[transformedWord] = [...map[transformedWord], currWord]
     } else {
-      map.set(transformedWord, [currWord]);
+      map[transformedWord] = [currWord];
     }
   }
-  
-  return Array.from(map.values());
+
+  return Object.values(map);
 };
