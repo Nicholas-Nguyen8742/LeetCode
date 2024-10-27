0b050e369568a7076d6a99e44b0c7eb6a359bb50
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-27 14:31:52 -0500

    Time: 27 ms (12.36%), Space: 57.6 MB (17.47%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  const map = new Map();
+  let max = {
+    value: -Infinity,
+    count: -Infinity,
+  };
+
+  nums.forEach((num) => {
+    const search = map.get(num);
+    const result = search != null ? search + 1 : 1;
+    if (max.count < result) {
+      max.value = num;
+      max.count = result
+    }
+    map.set(num, result);
+  });
+  
+  let result = [max.value];
+  map.delete(max.value);
+  let i = 1;
+  while (i < k) {
+    const removedElement = findNextMaxValue(map);
+    map.delete(removedElement);
+    result.push(removedElement);
+    i++;
+  }
+
+  return result;
+};
+
+var findNextMaxValue = function(map) {
+  let max = {
+    value: -Infinity,
+    count: -Infinity,
+  };
+
+  for (const [key, value] of map) {
+    if (max.count < value) {
+      max.value = key;
+      max.count = value;
+    }
+  }
+
+  return max.value;
+};
