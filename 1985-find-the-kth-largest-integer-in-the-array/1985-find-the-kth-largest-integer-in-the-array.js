9b3b49911c18eff5afc1224f9c82eaea21bea6ac
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-27 15:13:13 -0500

    Time: 193 ms (37.50%), Space: 65.1 MB (30.30%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
@@ -4,6 +4,13 @@
  * @return {string}
  */
 var kthLargestNumber = function(nums, k) {
-  const parsedNums = nums.map((x) => BigInt(x)).sort((a, b) => a >= b ? -1 : 1);
-  return parsedNums[k - 1].toString();
+  const parsedNums = nums.sort((a, b) => BigInt(a) >= BigInt(b) ? 1 : -1);
+  
+  let i = 1;
+  while (i < k) {
+    parsedNums.pop();
+    i++;
+  }
+
+  return `${parsedNums[parsedNums.length - 1]}`;
 };
