f82af05b18f622bce3711d3708a133f4ee6113de
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-27 14:36:26 -0500

    Time: 23 ms (22.22%), Space: 57.8 MB (25.37%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -0,0 +1,41 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  const map = createFrequencyMap(nums);
+  
+  let result = [];
+  let i = 0;
+  while (i < k) {
+    const removedElement = findNextMaxValue(map);
+    map.delete(removedElement);
+    result.push(removedElement);
+    i++;
+  }
+
+  return result;
+};
+
+var createFrequencyMap = function(array: number[]): Map<number, number> {
+  const map = new Map();
+
+  array.forEach((num) => {
+    const search = map.get(num);
+    map.set(num, search != null ? search + 1 : 1);
+  });
+
+  return map;
+};
+
+var findNextMaxValue = function(map: Map<number, number>): number {
+  let max = {
+    value: -Infinity,
+    count: -Infinity,
+  };
+
+  for (const [key, value] of map) {
+    if (max.count < value) {
+      max.value = key;
+      max.count = value;
+    }
+  }
+
+  return max.value;
+};
