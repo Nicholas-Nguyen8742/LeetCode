798ad5836ff2760b3905a07ef123414eeb51c3b1
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-28 14:37:04 -0500

    Time: 50 ms (100.00%), Space: 66.8 MB (78.85%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.js
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.js
@@ -6,17 +6,19 @@ var longestSquareStreak = function(nums) {
   let streak = -1;
   const set = new Set(nums);
 
-  for (let i = 0; i < nums.length; i++) {
+  let i = 0;
+  while (i < nums.length) {
     let curr = nums[i];
     let length = 0;
     while (set.has(curr)) {   
       length++;
       curr = curr ** 2;
     }
-    
+
     if (length > 1) {
       streak = Math.max(streak, length);      
     }
+    i++;
   }
 
   return streak;
