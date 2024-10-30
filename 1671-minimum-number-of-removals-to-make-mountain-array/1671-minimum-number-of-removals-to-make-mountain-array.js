30abb7f5d005f896265300896c81d1096c826e18
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-10-30 11:04:51 -0500

    Time: 87 ms (100.00%), Space: 53.1 MB (20.00%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumMountainRemovals = function(nums) {
+  const numsLength = nums.length;
+
+  const lisLength = Array(numsLength).fill(1);
+  const ldsLength = Array(numsLength).fill(1);
+
+  for (let i = 0; i < numsLength; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        lisLength[i] = Math.max(lisLength[i], lisLength[j] + 1);
+      }
+    }
+  }
+
+  for (let i = numsLength - 1; i >= 0; i--) {
+    for (let j = i + 1; j < numsLength; j++) {
+      if (nums[i] > nums[j]) {
+        ldsLength[i] = Math.max(ldsLength[i], ldsLength[j] + 1);
+      }
+    }
+  }
+
+  let minRemovals = Infinity;
+  for (let i = 0; i < numsLength; i++) {
+    if (lisLength[i] > 1 && ldsLength[i] > 1) {
+      minRemovals = Math.min(minRemovals, numsLength - lisLength[i] - ldsLength[i] + 1);
+    }
+  }
+
+  return minRemovals;
+};
