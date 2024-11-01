52be9d2fc54ab6251d18198f9a9557df6f22a22d
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-01 09:41:13 -0500

    Time: 73 ms (50.00%), Space: 61.2 MB (62.50%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.ts
@@ -0,0 +1,18 @@
+function makeFancyString(s: string): string {
+  let prev = s[0];
+  let result = s[0];
+  let consecutiveCount = 1;
+  const str = s.substring(1, s.length);
+  for (const char of str) {
+    if (char === prev) {
+      consecutiveCount++;
+    } else {
+      prev = char;
+      consecutiveCount = 1;
+    }
+    if (consecutiveCount < 3) {
+      result += char;
+    }
+  }
+  return result;
+};
