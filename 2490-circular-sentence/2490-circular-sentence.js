9a2196411b1c46c5986462b7e324a7bdad5a324b
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-02 02:36:15 -0500

    Time: 1 ms (6.25%), Space: 49.7 MB (18.18%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var isCircularSentence = function(sentence) {
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " " && sentence[i - 1] !== sentence[i + 1]) {
+      return false;
+    }
+  }
+  return sentence[0] === sentence[sentence.length - 1];
+};
