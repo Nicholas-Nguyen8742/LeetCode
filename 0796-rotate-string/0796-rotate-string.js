ebed321ccdf9cde8d58886939a71a9b08291f96f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-03 14:11:03 -0600

    Time: 0 ms (100.00%), Space: 49.2 MB (27.65%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,5 +4,17 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-  return s.length === goal.length && (s + s).includes(goal);
+  if (s.length != goal.length) {
+    return false;
+  }
+  
+  const length = s.length;
+  
+  for (let i = 0; i < length; i++) {
+    s = s.substring(1, length) + s.substring(0, 1);
+    if (s === goal) {
+      return true;
+    }
+  }
+  return false;
 };
