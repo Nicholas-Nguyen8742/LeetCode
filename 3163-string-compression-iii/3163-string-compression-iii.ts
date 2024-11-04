e8b87b65ace73a721a8c0e0fbcb7bbd4e745af73
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-03 19:41:09 -0600

    Time: 46 ms (22.22%), Space: 63.8 MB (100.00%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.ts
@@ -0,0 +1,24 @@
+function compressedString(word: string): string {
+  let result = '';
+  let currChar = word[0];
+  let currCount = 1;
+  for (let i = 1; i < word.length + 1; i++) {
+    const char = word[i];
+    if (char === currChar) {
+      if (currCount === 9) {
+        result += `${currCount}${currChar}`;
+        currCount = 1;
+      } else {
+        currCount++;
+      }
+      continue;
+    }
+
+    result += `${currCount}${currChar}`;
+
+    currChar = char;
+    currCount = 1;
+  }
+
+  return result;
+};
