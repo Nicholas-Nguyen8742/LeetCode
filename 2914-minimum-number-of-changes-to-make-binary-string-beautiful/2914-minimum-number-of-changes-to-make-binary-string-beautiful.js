877304c32984974fddcfea3cb4c6b0b3dd804490
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-05 16:26:07 -0600

    Time: 14 ms (50.00%), Space: 60.8 MB (12.50%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.js
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.js
@@ -2,14 +2,13 @@
  * @param {string} s
  * @return {number}
  */
-var minChanges = function(s) {
-  let minChangesRequired = 0;
-  
-  for (let i = 0; i < s.length; i += 2) {
-    if (s[i] != s[i + 1]) {
-      minChangesRequired += 1;
-    }
+var minChanges = function(s, minChangeCount = 0) {
+  if (s.length === 0) {
+    return minChangeCount;
+  }
+  if (s[0] != s[1]) {
+    minChangeCount += 1;
   }
 
-  return minChangesRequired;
+  return minChanges(s.substring(2, s.length), minChangeCount);
 };
