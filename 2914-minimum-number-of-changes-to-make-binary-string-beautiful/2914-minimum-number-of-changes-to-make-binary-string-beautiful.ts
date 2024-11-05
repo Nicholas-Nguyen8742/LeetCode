072039f58a0fa400afb41fb5527861b4a8329c92
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-05 16:22:20 -0600

    Time: 6 ms (100.00%), Space: 52.4 MB (100.00%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.ts
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function minChanges(s: string): number {
+  let curr = s[0];
+  let consecutiveCount = 0;
+  let minChangesRequired = 0;
+  
+  for (const char of s) {
+    if (char === curr) {
+      consecutiveCount += 1;
+      continue;
+    }
+    
+    if (consecutiveCount % 2 === 0) {
+      consecutiveCount = 1;
+    } else {
+      consecutiveCount = 0;
+      minChangesRequired += 1;
+    }
+    
+    curr = char;
+  }
+
+  return minChangesRequired;
+};
