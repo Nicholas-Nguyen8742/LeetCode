ae88c9913747ba70337252a2487dd116185c7ce7
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-07 16:42:14 -0600

    Time: 42 ms (100.00%), Space: 59.5 MB (100.00%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.ts
@@ -0,0 +1,15 @@
+function largestCombination(candidates: number[]): number {
+  let max = 0;
+  
+  for (let i = 0; i < 24; i++) {
+    let currCount = 0;
+    for (const num of candidates) {
+      if (num & (1 << i)) {
+        currCount += 1;
+      }
+    }
+    max = Math.max(max, currCount);
+  }
+
+  return max;
+};
