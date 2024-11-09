f120f174708b8b13902642090a06c34ab44f83b0
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-09 16:00:22 -0600

    Time: 117 ms (10.51%), Space: 52 MB (86.90%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i === j) continue;
+   for (let j = 0; j < nums.length; j++) {
+     if (i === j) continue;
 
-       if (nums[j] + nums[i] === target) {
-        return [i, j];
-       }
-    }
-  }
-  return [0,0];
+     if (nums[j] + nums[i] === target) {
+       return [i, j];
+     }
+   }
+ }
 };
