0ddac6384aa6165905abedcfce3dad1f9d7f3046
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-15 17:57:20 -0600

    Time: 5 ms (100.00%), Space: 61.2 MB (85.71%) - LeetHub

---
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.ts
@@ -0,0 +1,26 @@
+function findLengthOfShortestSubarray(arr: number[]): number {
+  let right = arr.length - 1;
+
+  while (right > 0 && arr[right] >= arr[right - 1]) {
+    right -= 1;
+  }
+
+  if (right === 0) {
+    return 0;
+  }
+
+  let ans = right;
+  let left = 0;
+
+  while (left < right && (left === 0 || arr[left - 1] <= arr[left])) {
+    while (right < arr.length && arr[left] > arr[right]) {
+      right += 1;
+    }
+
+    ans = Math.min(ans, right - left - 1);
+
+    left += 1;
+  }
+
+  return ans;
+};
