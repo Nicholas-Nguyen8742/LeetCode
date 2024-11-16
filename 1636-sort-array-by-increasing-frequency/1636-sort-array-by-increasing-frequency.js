3d3f1cf126010ab25ce6231c2cabe445da4a8050
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-16 15:08:48 -0600

    Time: 6 ms (74.16%), Space: 51.8 MB (74.87%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -3,39 +3,32 @@
  * @return {number[]}
  */
 var frequencySort = function(nums) {
-  const map = new Map();
   let indexes = { };
   let result = [];
 
   for (let i = 0; i < nums.length; i++) {
     const key = nums[i];
-    const value = map.get(key);
-    if (value != null) {
-      const prev = result[indexes[key]];
-      const newValue = value + 1;
+    const element = key in indexes && result[indexes[key]];
+    if (element) {
+      const newValue = element.value + 1;
       result[indexes[key]] = {
-        ...prev,
+        key,
         value: newValue
       }
-      map.set(key, newValue);
     } else {
       result.push({ key, value: 1 });
       indexes[key] = result.length - 1;
-      map.set(key, 1);
     }
   }
 
-  return result.sort(sortComparator).flatMap(fillElement);
-};
-
-var sortComparator = function(a, b) {
-  if (a.value === b.value) {
-    return b.key - a.key; 
-  } else {
-    return a.value - b.value;
-  }
-}
-
-var fillElement = function(el) {
-  return new Array(el.value).fill(el.key);
+  return result
+    .sort(function(a, b) {
+      if (a.value === b.value) {
+        return b.key - a.key; 
+      }
+      return a.value - b.value;
+    })
+    .flatMap(function(el) {
+      return new Array(el.value).fill(el.key);
+    });
 };
