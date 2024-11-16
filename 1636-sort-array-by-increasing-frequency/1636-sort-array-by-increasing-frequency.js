603f91d3db15ab5ee7d3e6febf43c2cdd8c35671
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-16 15:01:26 -0600

    Time: 8 ms (54.49%), Space: 51.7 MB (85.45%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -4,22 +4,27 @@
  */
 var frequencySort = function(nums) {
   const map = new Map();
+  let indexes = { };
+  let result = [];
+
   for (let i = 0; i < nums.length; i++) {
     const key = nums[i];
     const value = map.get(key);
-    console.log(value);
-    
-    value != null ? map.set(key, value + 1) : map.set(key, 1);
+    if (value != null) {
+      const currIndex = indexes[key];
+      const prev = result[currIndex];
+      const newValue = value + 1;
+      result[currIndex] = {
+        ...prev,
+        value: newValue
+      }
+      map.set(key, newValue);
+    } else {
+      result.push({ key, value: 1 });
+      indexes[key] = result.length - 1;
+      map.set(key, 1);
+    }
   }
-  
-  return toArray(map);
-};
-
-var toArray = function(map) {
-  let result = [];
-  map.forEach((value, key) => {
-    result.push({ key, value });
-  });
 
   return result.sort(sortComparator).flatMap(fillElement);
 };
