4cf72cb2202963dc9dd6b4ad1dd8fb00dcfd39ce
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-16 14:16:04 -0600

    Time: 4 ms (71.43%), Space: 51.9 MB (100.00%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.js
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.js
@@ -6,20 +6,25 @@
 var resultsArray = function(nums, k) {
   const length = nums.length;
   let result = new Array(length - k + 1).fill(0);
-  
-  for (let start = 0; start <= length - k; start++) {
+  let start = 0;
+
+  while (start <= length - k) {
     let isConsecutiveSorted = true;
-    for (let j = start; j < start + k - 1; j++) {
+    let j = start;
+    while (j < start + k - 1) {
       if (nums[j + 1] != nums[j] + 1) {
         isConsecutiveSorted = false;
         break;
       }
+      j++;
     }
+
     if (isConsecutiveSorted) {
       result[start] = nums[start + k - 1];
     } else {
       result[start] = -1;
     }
+    start++;
   }
 
   return result;
