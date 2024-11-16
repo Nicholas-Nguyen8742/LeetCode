4668993edaf0ca46be9e3d82ebb6dc8cbeca10d3
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-16 16:04:23 -0600

    Time: 1 ms (100.00%), Space: 52 MB (81.82%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.ts
@@ -0,0 +1,14 @@
+function mostFrequent(nums: number[], key: number): number {
+  let freq = { };
+  let result = 0;
+  for (let i = 0, max = 0; i < nums.length - 1; ++i) {
+    if (nums[i] !== key) continue;
+    const target = nums[i + 1];
+    freq[target] = (freq[target] || 0) + 1;
+    if (freq[target] > max) {
+      max = freq[target];
+      result = target;
+    }
+  }
+  return result;
+};
