d2bcda17d93bbb55fe69b6de39f4fcc6231fc680
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-19 17:01:24 -0600

    Time: 58 ms (64.53%), Space: 83.9 MB (5.49%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximumSubarraySum = function(nums, k) {
+  let ans = 0;
+  let currentSum = 0;
+  let begin = 0;
+  let end = 0;
+  const numToIndex = new Map();
+
+  while (end < nums.length) {
+    const currNum = nums[end];
+    const lastOccurrence = numToIndex.has(currNum) ? numToIndex.get(currNum) : -1;
+
+    while (begin <= lastOccurrence || end - begin + 1 > k) {
+      currentSum -= nums[begin];
+      begin++;
+    }
+
+    numToIndex.set(currNum, end);
+    currentSum += nums[end];
+
+    if (end - begin + 1 === k) {
+      ans = Math.max(ans, currentSum);
+    }
+
+    end++;
+  }
+
+  return ans;
+};
