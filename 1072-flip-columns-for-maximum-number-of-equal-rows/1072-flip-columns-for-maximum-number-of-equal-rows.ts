dc82701cdf8aa00369db62c102a94fa18f53a0de
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-22 16:29:45 -0600

    Time: 38 ms (85.71%), Space: 59.9 MB (50.00%) - LeetHub

---
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.ts
@@ -0,0 +1,13 @@
+function maxEqualRowsAfterFlips(matrix: number[][]): number {
+  const patternFrequency = new Map();
+
+  for (const row of matrix) {
+    const pattern = row
+      .map((num) => num === row[0] ? 'T' : 'F')
+      .join('');
+
+    patternFrequency.set(pattern, (patternFrequency.get(pattern) || 0) + 1);
+  }
+
+  return Math.max(...patternFrequency.values());
+};
