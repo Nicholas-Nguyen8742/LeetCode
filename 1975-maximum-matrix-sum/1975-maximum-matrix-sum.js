eb7ff8f5fd8e1838bdd42585e36638ae323bcb07
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-24 14:50:31 -0600

    Time: 11 ms (14.29%), Space: 60.4 MB (10.00%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxMatrixSum = function(matrix) {
+  let totalSum = 0;
+  let minVal = Infinity;
+  let negativeCount = 0;
+  for (const row of matrix) {
+    for (const value of row) {
+      totalSum += Math.abs(value);
+      if (value < 0) {
+        negativeCount += 1;
+      }
+      
+      minVal = Math.min(minVal, Math.abs(value));
+     }
+  }
+  if (negativeCount % 2 !== 0) {
+    totalSum -= 2 * minVal;
+  }
+  return totalSum;
+};
