90d6731a659b2579069dc05c87b43e8a46b0b3bf
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-25 10:06:08 -0600

    Time: 8 ms (100.00%), Space: 56.4 MB (70.00%) - LeetHub

---
+++ 0773-sliding-puzzle/0773-sliding-puzzle.ts
@@ -0,0 +1,43 @@
+function slidingPuzzle(board: number[][]): number {
+  const target = '123450';
+  const directions = [
+    [1, 3],
+    [0, 2, 4],
+    [1, 5],
+    [0, 4],
+    [1, 3, 5],
+    [2, 4],
+  ];
+  const startState = board.flat().join("");
+
+  const visited = new Set();
+  const queue: [string, number][] = [[startState, 0]];
+  visited.add(startState);
+
+  while (queue.length > 0) {
+    const [currentState, moves] = queue.shift();
+
+    if (currentState === target) {
+      return moves;
+    }
+
+    const zeroPos = currentState.indexOf("0");
+
+    for (const newPos of directions[zeroPos]) {
+      const nextState = swap(currentState, zeroPos, newPos);
+
+      if (!visited.has(nextState)) {
+        visited.add(nextState);
+        queue.push([nextState, moves + 1]);
+      }
+    }
+  }
+
+  return -1;
+};
+
+function swap(state: string, i: number, j: number): string {
+  const splitArray = state.split("");
+  [splitArray[i], splitArray[j]] = [splitArray[j], splitArray[i]];
+  return splitArray.join("");
+};
