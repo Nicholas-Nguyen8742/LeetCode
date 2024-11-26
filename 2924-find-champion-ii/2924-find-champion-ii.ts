aa4b15d6a2bf9d1deea7f37cfbab74259570d7bd
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-11-26 09:29:55 -0600

    Time: 1 ms (100.00%), Space: 58.4 MB (100.00%) - LeetHub

---
+++ 2924-find-champion-ii/2924-find-champion-ii.ts
@@ -0,0 +1,18 @@
+function findChampion(n: number, edges: number[][]): number {
+  let indegree = new Array(n).fill(0);
+  for (const [winner, loser] of edges) {
+    indegree[loser]++;
+  }
+
+  let champ = -1;
+  let champCount = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (indegree[i] === 0) {
+      champCount++;
+      champ = i;
+    }
+  }
+
+  return champCount === 1 ? champ : -1;
+};
