fd536d9ea4bba9106e0b77e92c0db3fce60192c2
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-03 15:00:38 -0600

    Time: 61 ms (85.96%), Space: 86.8 MB (80.00%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
@@ -6,10 +6,13 @@
 var addSpaces = function(s, spaces) {
   let result = '';
   let lastIndex;
+
   for (let i = 0; i <= spaces.length; i++) {
     const index = spaces[i];
-    result = result + (lastIndex == null ? '' : ' ') + s.slice((lastIndex == null ? 0 : lastIndex), index);
-    lastIndex = index || 0;
+    const separator = lastIndex == null ? '' : ' ';
+    const startIndex = lastIndex == null ? 0 : lastIndex;
+    result = result + separator + s.slice(startIndex, index);
+    lastIndex = index;
   }
 
   return result;
