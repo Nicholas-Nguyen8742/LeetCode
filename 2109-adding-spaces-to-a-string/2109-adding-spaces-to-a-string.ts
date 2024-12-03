6a95cafa1ba434646c4c3e0b3f4469f053f5009c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-03 17:28:49 -0600

    Time: 55 ms (75.00%), Space: 85.2 MB (100.00%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.ts
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.ts
@@ -1,11 +1,7 @@
 function addSpaces(s: string, spaces: number[]): string {
-  let result: string = '';
-  let lastIndex: number | undefined;
+  let result: string = '', lastIndex: number | undefined;
 
-  for (let i = 0; i <= spaces.length; i++) {
-    result += (lastIndex == null ? '' : ' ') + s.slice((lastIndex == null ? 0 : lastIndex), spaces[i]);
-    lastIndex = spaces[i];
-  }
+  for (let i = 0; i <= spaces.length; i++) result += (lastIndex == null ? '' : ' ') + s.slice((lastIndex == null ? 0 : lastIndex), spaces[i]), lastIndex = spaces[i];
 
   return result;
 };
