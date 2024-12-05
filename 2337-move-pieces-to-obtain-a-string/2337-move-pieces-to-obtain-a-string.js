233d0907cb95446aa3ee18a2ed0d08abffd2705c
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-05 11:14:52 -0600

    Time: 131 ms (15.00%), Space: 88.5 MB (7.41%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} start
+ * @param {string} target
+ * @return {boolean}
+ */
+var canChange = function(start, target) {
+  const startQueue = [];
+  const targetQueue = [];
+  
+  for (let i = 0; i < start.length; i++) {
+    if (start[i] != '_') {
+      startQueue.push([start[i], i]);
+    }
+    
+    if (target[i] != '_') {
+      targetQueue.push([target[i], i]);
+    }
+  }
+  
+  if (startQueue.length != targetQueue.length) {
+    return false;
+  }
+
+  while (startQueue.length != 0) {
+    const [startChar, startIndex] = startQueue.pop();
+    const [targetChar, targetIndex] = targetQueue.pop();
+    
+    if (
+      startChar != targetChar
+      || (startChar === 'L' && startIndex < targetIndex)
+      || (startChar === 'R' && startIndex > targetIndex)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+};
