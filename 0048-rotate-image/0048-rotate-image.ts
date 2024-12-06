522fdd3be6118d896cb3d6eea1574fccfb658774
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-06 14:43:56 -0600

    Time: 0 ms (100.00%), Space: 51.4 MB (80.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.ts
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -5,9 +5,10 @@ function rotate(matrix: number[][]): void {
   const matrixRef = structuredClone(matrix);
   const LENGTH = matrix.length;
 
-  for (let refColumnIndex = matrix[0].length - 1, rowIndex = 0; rowIndex < LENGTH; rowIndex++, refColumnIndex--) {
-    for (let refRowIndex = 0, columnIndex = 0; columnIndex < LENGTH; columnIndex++, refRowIndex++) {
-      matrix[refRowIndex][refColumnIndex] = matrixRef[rowIndex][columnIndex];
+  for (let rowIndex = 0; rowIndex < LENGTH; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < LENGTH; columnIndex++) {
+      const refColumnIndex = Math.abs(LENGTH - 1 - rowIndex);
+      matrix[columnIndex][refColumnIndex] = matrixRef[rowIndex][columnIndex];
     }
   }
 };
