175e13b9fdf012ad5f149770b9cb27d7508c12db
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-07 14:41:29 -0600

    Time: 0 ms (100.00%), Space: 51.9 MB (77.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -5,16 +5,10 @@
 var repeatedNTimes = function(nums) {
   let hash = {};
   for (const num of nums) {
-    if (isNotNull(hash[num])) {
-      return num;
-    }
-
+    if (isNotNull(hash[num])) return num;
     hash[num] = num;
   }
-
   return -1;
 };
 
-var isNotNull = function(value) {
-  return value != null;
-};
+var isNotNull = function(value) { return value != null };
