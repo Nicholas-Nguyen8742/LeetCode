d761c255229cf466b412c508ea2979cc0057e6be
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-07 13:05:28 -0600

    Time: 43 ms (51.85%), Space: 60 MB (78.13%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} maxOperations
+ * @return {number}
+ */
+var minimumSize = function(nums, maxOperations, max = 0) {
+  let left = 1, right = Math.max(...nums);
+
+  while (left < right) {
+    const middle = Math.floor((left + right) / 2);
+    if (isPossible(middle, nums, maxOperations)) {
+      right = middle;
+    } else {
+      left = middle + 1; // Try a larger value
+    }
+  }
+
+  return left;
+};
+
+var isPossible = function(maxCurrBalls, nums, maxOperations) {
+  let total = 0;
+
+  for (const num of nums) {
+    total += Math.ceil(num / maxCurrBalls) - 1;
+    if (total > maxOperations) {
+      return false;
+    }
+  }
+  return true;
+};
