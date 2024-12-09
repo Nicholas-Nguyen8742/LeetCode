33bcaeb59d0195c6cf705f20f521f9f0fdefb604
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-09 14:16:39 -0600

    Time: 29 ms (60.00%), Space: 85 MB (33.33%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.ts
@@ -0,0 +1,15 @@
+function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
+  const intervals = [0];
+
+  for (let i = 1; i < nums.length; i++) {
+    intervals.push(intervals[i - 1] + (nums[i - 1] % 2 === nums[i] % 2 ? 1 : 0));
+  }
+
+  const result = [];
+
+  for (const [start, end] of queries) {
+    result.push(intervals[start] === intervals[end]);
+  }
+
+  return result;
+};
