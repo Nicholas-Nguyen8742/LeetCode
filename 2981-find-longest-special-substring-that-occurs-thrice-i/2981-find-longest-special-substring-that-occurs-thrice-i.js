ceebfc1031d3de76125161f5d3fc7524b6e38928
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-10 09:48:37 -0600

    Time: 22 ms (56.25%), Space: 57.2 MB (21.05%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maximumLength = function(s) {
+  const count = new Map();
+
+  for (let start = 0; start < s.length; start++) {
+    const currString = [];
+
+    for (let end = start; end < s.length; end++) {
+      if (currString.length === 0 || currString[currString.length - 1] === s[end]) {
+        currString.push(s[end]);
+        const currToString = currString.join("");
+
+        count.set(currToString, (count.get(currToString) || 0) + 1);
+      } else {
+        break;
+      }
+    }
+  }
+
+  let ans = -1;
+  for (const [str, freq] of count.entries()) {
+    if (freq >= 3 && str.length > ans) {
+      ans = str.length;
+    }
+  }
+
+  return ans;
+};
