b57d62f9e11a2594aa7fca367c733d52582467ee
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-12 18:54:17 -0600

    Time: 296 ms (86.67%), Space: 85.3 MB (70.59%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findScore = function(nums) {
+  let score = 0;
+  const sorted = nums.map((num, idx) => [num, idx]);
+  sorted.sort((a, b) => a[0] - b[0]);
+
+  const marked = Array(nums.length).fill(false);
+
+  for (let i = 0; i < nums.length; i++) {
+    const [number, index] = sorted[i];
+    if (!marked[index]) {
+      score += number;
+      marked[index] = true;
+
+      if (index - 1 >= 0) marked[index - 1] = true;
+      if (index + 1 < nums.length) marked[index + 1] = true;
+    }
+  }
+
+  return score;
+};
