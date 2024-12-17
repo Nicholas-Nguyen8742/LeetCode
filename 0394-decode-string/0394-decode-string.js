3f425d5ca6a3da481c6cec3e6f93b6f82c3d4f37
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-17 16:48:42 -0600

    Time: 0 ms (100.00%), Space: 49.1 MB (21.39%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+  let stack = [];
+  let resultString = '';
+  let currentNumber = 0;
+
+  for (const char of s) {
+    if (!isNaN(char)) {
+      currentNumber = currentNumber * 10 + parseInt(char);
+    } else if (char === '[') {
+      stack.push([resultString, currentNumber]);
+      resultString = '';
+      currentNumber = 0;
+    } else if (char === ']') {
+      const [prevString, repeatTimes] = stack.pop();
+      resultString = prevString + resultString.repeat(repeatTimes);
+    } else {
+      resultString += char;
+    }
+  }
+
+  return resultString;
+};
