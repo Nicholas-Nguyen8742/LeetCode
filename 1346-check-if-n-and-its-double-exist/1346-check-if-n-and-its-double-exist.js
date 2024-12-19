f7734dfe1fa1a42b7848bbb786b0ffd521e2ffb0
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-19 13:02:47 -0600

    Time: 1 ms (81.65%), Space: 49.3 MB (87.23%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -6,10 +6,7 @@ var checkIfExist = function(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (i === j) continue;
-      
-      if (arr[i] === 2 * arr[j]) {
-        return true;
-      }
+      if (arr[i] === 2 * arr[j]) return true;
     }
   }
 
