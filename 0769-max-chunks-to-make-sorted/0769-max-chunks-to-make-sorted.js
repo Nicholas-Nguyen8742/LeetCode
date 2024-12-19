4e970288fad37972cc5bca56745d26ce9105ad7f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-19 12:55:23 -0600

    Time: 0 ms (100.00%), Space: 48.9 MB (52.90%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.js
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.js
@@ -5,8 +5,6 @@
 var maxChunksToSorted = function(arr) {
   let chunks = 0, max = 0;
   
-  for (let i = 0; i < arr.length; i++) {
-    max = Math.max(arr[i], max); if (max === i) chunks++;
-  }
+  for (let i = 0; i < arr.length; i++) { max = Math.max(arr[i], max); if (max === i) chunks++; }
   return chunks;
 };
