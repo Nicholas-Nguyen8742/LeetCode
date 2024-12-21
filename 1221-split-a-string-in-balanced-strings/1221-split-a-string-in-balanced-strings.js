4739f51785a1fc899e236d29f128ecb0dee75d7a
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2024-12-21 16:47:12 -0600

    Time: 1 ms (34.59%), Space: 49.6 MB (17.12%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-  let balanceCount = 0, result = 0, i = 0;
-
-  while (i < s.length) {
-    s[i] === 'L' ? balanceCount++ : balanceCount--;
-
+  let balanceCount = 0, result = 0;
+  
+  for (let i = 0, increment = (char) => char === 'L' ? balanceCount++ : balanceCount--; i < s.length; i++) {
+    increment(s[i]);
     if (balanceCount === 0) result++;
-    
-    i++;
-  }
+  } 
   
   return result;
 };
