18571321c881d9d1b61d9841dfdeba1891aadd28
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-01-01 21:41:32 -0600

    Time: 10 ms, Memory: 79.5 MB - LeetHub

---
+++ 0545-33-2559-count-vowel-strings-in-ranges/0545-33-2559-count-vowel-strings-in-ranges.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} words
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var vowelStrings = function(words, queries) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    const result = new Array(queries.length).fill(0);
+    const prefixSum = new Array(words.length).fill(0);
+
+    let sum = 0;
+    for (let i = 0; i < words.length; i++) {
+      const currentWord = words[i];
+      if (
+        vowels.has(currentWord[0]) &&
+        vowels.has(currentWord[currentWord.length - 1])
+      ) {
+        sum += 1;
+      }
+      prefixSum[i] = sum;
+    }
+
+    for (let i = 0; i < queries.length; i++) {
+      const [start, end] = queries[i];
+      result[i] = prefixSum[end] - (start === 0 ? 0 : prefixSum[start - 1]);
+    }
+
+    return result;
+};
