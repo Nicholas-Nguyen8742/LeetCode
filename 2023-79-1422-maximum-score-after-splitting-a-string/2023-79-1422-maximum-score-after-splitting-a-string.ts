c5bd64b24c5d8eee147fd389aa1932b657c31903
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-01-01 19:32:27 -0600

    Time: 0 ms, Memory: 51.7 MB - LeetHub

---
+++ 2023-79-1422-maximum-score-after-splitting-a-string/2023-79-1422-maximum-score-after-splitting-a-string.ts
@@ -0,0 +1,23 @@
+function maxScore(s: string): number {
+    let ones = 0;
+    let zeros = 0;
+    let best = -Infinity;
+
+    const lastIndex = s.length - 1;
+
+    for (let i = 0; i < lastIndex; i++) {
+        if (s[i] == '0') {
+            zeros += 1;
+        } else {
+            ones += 1;
+        }
+
+        best = Math.max(best, zeros - ones);
+    }
+
+    if (s[s.length - 1] == '1') {
+        ones += 1;
+    }
+
+    return best + ones;
+};
