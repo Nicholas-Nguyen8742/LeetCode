81ada803f6041d8e10f32283fc802af2abf9e0fe
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-01-07 21:42:46 -0600

    Time: 2 ms, Memory: 51.8 MB - LeetHub

---
+++ 0184-15-3042-count-prefix-and-suffix-pairs-i/0184-15-3042-count-prefix-and-suffix-pairs-i.ts
@@ -0,0 +1,23 @@
+function countPrefixSuffixPairs(words: string[]): number {
+    const length = words.length;
+    let count = 0;
+
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (i === j) continue;
+    
+            const str1 = words[i];
+            const str2 = words[j];
+
+            if (str1.length > str2.length) {
+                continue;
+            }
+
+            if (str2.startsWith(str1) && str2.endsWith(str1)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+};
