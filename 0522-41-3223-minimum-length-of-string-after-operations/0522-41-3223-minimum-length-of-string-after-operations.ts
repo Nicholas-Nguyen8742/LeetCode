2437d8f67cd47f6eed406e2b8b05cebb9275e3e6
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-01-13 10:51:15 -0600

    Time: 209 ms, Memory: 64.9 MB - LeetHub

--- 0522-41-3223-minimum-length-of-string-after-operations/0522-41-3223-minimum-length-of-string-after-operations.ts
+++ 0522-41-3223-minimum-length-of-string-after-operations/0522-41-3223-minimum-length-of-string-after-operations.ts
@@ -1,22 +1,22 @@
 function minimumLength(s: string): number {
-    const frequency = new Map();
-    let toDeleteCount = 0;
+  const frequency = new Map();
+  let toDeleteCount = 0;
 
-    for (const char of s) {
-        if (frequency.has(char)) {
-            frequency.set(char, frequency.get(char) + 1);
-        } else {
-            frequency.set(char, 1);
-        }
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    const value = (frequency.get(char) || 0) + 1;
+    
+    frequency.set(char, value);
+
+    if (!(s.substring(i + 1).includes(char))) {
+      if (value % 2 === 1) {
+        toDeleteCount += (value - 1);
+      } else {
+        toDeleteCount += (value - 2);
+      }
     }
 
-    for (const value of frequency.values()) {
-        if (value % 2 === 1) {
-            toDeleteCount += (value - 1);
-        } else {
-            toDeleteCount += (value - 2);
-        }
-    }
+  }
 
-    return s.length - toDeleteCount;
+  return s.length - toDeleteCount;
 };
