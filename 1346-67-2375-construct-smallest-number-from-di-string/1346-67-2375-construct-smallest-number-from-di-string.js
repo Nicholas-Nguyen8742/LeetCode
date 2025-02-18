7f2a57a3c921a1e699af829576d4c46ebe07b12a
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-02-18 08:46:27 -0600

    Time: 0 ms, Memory: 53.9 MB - LeetHub

--- 1346-67-2375-construct-smallest-number-from-di-string/1346-67-2375-construct-smallest-number-from-di-string.js
+++ 1346-67-2375-construct-smallest-number-from-di-string/1346-67-2375-construct-smallest-number-from-di-string.js
@@ -3,21 +3,23 @@
  * @return {string}
  */
 var smallestNumber = function(pattern) {
-  const result = [];
-  var buildSequence = function(currentIndex, currentCount, p) {
-    if (currentIndex != p.length) {
-      if (p[currentIndex] === 'I') {
-        buildSequence(currentIndex + 1, currentIndex + 1, p);
-      } else {
-        currentCount = buildSequence(currentIndex + 1, currentCount, p);
-      }
+  const sm = Object.create(smallestNumber.prototype);
+  sm.result = [];
+
+  sm.buildSequence(0, 0, pattern);
+
+  return sm.result.reverse().join('');
+};
+
+smallestNumber.prototype.buildSequence = function(currentIndex, currentCount, p) {
+  if (currentIndex != p.length) {
+    if (p[currentIndex] === 'I') {
+      this.buildSequence(currentIndex + 1, currentIndex + 1, p);
+    } else {
+      currentCount = this.buildSequence(currentIndex + 1, currentCount, p);
     }
+  }
 
-    result.push(currentCount + 1);
-    return currentCount + 1;
-  };
-
-  buildSequence(0, 0, pattern);
-
-  return result.reverse().join('');
+  this.result.push(currentCount + 1);
+  return currentCount + 1;
 };
