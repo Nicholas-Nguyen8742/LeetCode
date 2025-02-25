d3a481fb40b856a5c376fbb29625097bf1d000dd
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-02-25 16:24:29 -0600

    Time: 6 ms, Memory: 65.1 MB - LeetHub

---
+++ 1925-92-1524-number-of-sub-arrays-with-odd-sum/1925-92-1524-number-of-sub-arrays-with-odd-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var numOfSubarrays = function(arr) {
+  let modulo =  10 ** 9 + 7
+  let count = 0, prefixSum = 0, oddCount = 0, evenCount = 1;
+
+  for (const num of arr) {
+    prefixSum += num;
+
+    if (prefixSum % 2 === 0) {
+      count += oddCount;
+      evenCount += 1;
+    } else {
+      count += evenCount;
+      oddCount += 1;
+    }
+
+    count %= modulo;
+  }
+  return count;
+};
