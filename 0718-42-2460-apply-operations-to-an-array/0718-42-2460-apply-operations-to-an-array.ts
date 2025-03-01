e84f4c6dde52a64670ba2ca184e6547482d8eac7
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-01 00:26:11 -0600

    Time: N/A, Memory: N/A',
      },
      questionId: '2551',
      submissionId: '1558852427',
      sessionId: '6978111',
      getLangDisplay: 'typescript',
      submissionCode: 'function applyOperations(nums: number[]): number[] {\u000A  let zeros \u003D 0\u003B\u000A  const result \u003D []\u003B\u000A\u000A  for (let i \u003D 0\u003B i \u003C nums.length\u003B i++) {\u000A    if (nums[i] \u003D\u003D\u003D nums[i + 1]) {\u000A      nums[i] \u003D nums[i] * 2\u003B\u000A      nums[i + 1] \u003D 0\u003B\u000A    }\u000A\u000A    if (nums[i] \u003D\u003D\u003D 0) {\u000A      zeros++\u003B\u000A    } else {\u000A      result.push(nums[i])\u003B\u000A    }\u000A  }\u000A\u000A  return result.concat(new Array(zeros).fill(0))\u003B\u000A}\u003B',
      editCodeUrl: '/problems/apply-operations-to-an-array/',
      checkUrl: '/submissions/detail/1558852427/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022typescript\u0022, \u0022distribution\u0022: [[\u00220\u0022, 16.6667], [\u00221\u0022, 20.3704], [\u00222\u0022, 20.3704], [\u00223\u0022, 11.1111], [\u00224\u0022, 5.5556], [\u00225\u0022, 7.4074], [\u00226\u0022, 3.7037], [\u00227\u0022, 1.8519], [\u00228\u0022, 1.8519], [\u002211\u0022, 1.8519], [\u002212\u0022, 1.8519]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022typescript\u0022, \u0022distribution\u0022: [[\u002252500\u0022, 1.8519], [\u002252600\u0022, 7.4074], [\u002252700\u0022, 11.1111], [\u002252800\u0022, 7.4074], [\u002252900\u0022, 5.5556], [\u002253000\u0022, 12.963], [\u002253100\u0022, 5.5556], [\u002253200\u0022, 1.8519], [\u002253300\u0022, 1.8519], [\u002253400\u0022, 5.5556], [\u002253700\u0022, 1.8519], [\u002258400\u0022, 1.8519], [\u002258500\u0022, 3.7037], [\u002258600\u0022, 1.8519], [\u002258700\u0022, 1.8519], [\u002258800\u0022, 3.7037], [\u002258900\u0022, 1.8519], [\u002259000\u0022, 5.5556], [\u002259100\u0022, 5.5556], [\u002259300\u0022, 1.8519], [\u002259400\u0022, 1.8519], [\u002259500\u0022, 1.8519]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ 0718-42-2460-apply-operations-to-an-array/0718-42-2460-apply-operations-to-an-array.ts
@@ -0,0 +1,19 @@
+function applyOperations(nums: number[]): number[] {
+  let zeros = 0;
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = nums[i] * 2;
+      nums[i + 1] = 0;
+    }
+
+    if (nums[i] === 0) {
+      zeros++;
+    } else {
+      result.push(nums[i]);
+    }
+  }
+
+  return result.concat(new Array(zeros).fill(0));
+};
