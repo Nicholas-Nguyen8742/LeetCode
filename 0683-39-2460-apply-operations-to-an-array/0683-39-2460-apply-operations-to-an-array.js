2cfb72b70ad37843c49420226b7d9a20da10f46f
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-02-28 23:08:38 -0600

    Time: 2 ms, Memory: 58.3 MB - LeetHub

---
+++ 0683-39-2460-apply-operations-to-an-array/0683-39-2460-apply-operations-to-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+  const zeros = [];
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i];
+    const next = nums[i + 1];
+    if (curr === next) {
+      curr = curr * 2;
+      nums[i + 1] = 0;
+    }
+
+    if (curr === 0) {
+      zeros.push(0);
+    } else {
+      result.push(curr);
+    }
+  }
+
+  return [...result, ...zeros];
+};
