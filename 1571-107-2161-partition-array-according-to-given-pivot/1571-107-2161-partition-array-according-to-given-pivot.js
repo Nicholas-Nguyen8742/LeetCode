99a07b71a0ff56a9b1b423086585e7aaaa096a0e
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-03 13:17:34 -0600

    Time: 47 ms, Memory: 87.1 MB - LeetHub

---
+++ 1571-107-2161-partition-array-according-to-given-pivot/1571-107-2161-partition-array-according-to-given-pivot.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function(nums, pivot) {
+  let pivots = 0;
+  const left = [], right = [];
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    if (pivot === currentNum) {
+      pivots++;
+    } else if (currentNum < pivot) {
+      left.push(currentNum);
+    } else {
+      right.push(currentNum);
+    }
+  }
+
+  return [...left, ...Array(pivots).fill(pivot), ...right];
+};
