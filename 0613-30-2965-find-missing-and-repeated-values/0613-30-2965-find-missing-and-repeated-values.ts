f9baf5a9d96530f500f747f5ff7791cd2620d559
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-06 11:22:34 -0600

    Time: 7 ms, Memory: 62.1 MB - LeetHub

---
+++ 0613-30-2965-find-missing-and-repeated-values/0613-30-2965-find-missing-and-repeated-values.ts
@@ -0,0 +1,25 @@
+function findMissingAndRepeatedValues(grid: number[][]): number[] {
+  const result = [];
+  const set = createNumberSet(grid.length ** 2);
+
+  const flatGrid = grid.flat();
+  for (let i = 0; i < flatGrid.length; i++) {
+    const curr = flatGrid[i];
+    const hasCurr = set.has(curr);
+    if (hasCurr) {
+      set.delete(curr)
+    } else {
+      result.push(curr);
+    }
+  }
+
+  return result.concat([...set]);
+};
+
+function createNumberSet(n: number) {
+  const numberSet = new Set();
+  for (let i = 1; i <= n; i++) {
+    numberSet.add(i);
+  }
+  return numberSet;
+}
