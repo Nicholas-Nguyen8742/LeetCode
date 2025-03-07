d1c82632ed51d9574567e465cf1ea33d6bce5fca
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-06 19:13:40 -0600

    Time: 1092 ms, Memory: 64.5 MB - LeetHub

---
+++ 0403-32-2523-closest-prime-numbers-in-range/0403-32-2523-closest-prime-numbers-in-range.ts
@@ -0,0 +1,44 @@
+function closestPrimes(left: number, right: number): number[] {
+  let result = [-1, -1];
+  let primes = [];
+
+  for (let candidate = left; candidate <= right; candidate++) {
+    if (candidate % 2 == 0 && candidate > 2) {
+      continue;
+    }
+
+    const primesLength = primes.length;
+
+    if (isPrime(candidate)) {
+      if (primesLength && candidate <= primes[primesLength] + 2) {
+        return [primes[primesLength], candidate];
+      }
+      primes.push(candidate);
+    }
+  }
+
+  if (primes.length < 2) {
+    return [-1, -1];
+  }
+
+  let minDiff = 1452;
+
+  for (let i = 1; i < primes.length; i++) {
+    const currDiff = primes[i] - primes[i - 1];
+    if (currDiff < minDiff) {
+      minDiff = currDiff;
+      result = [primes[i - 1], primes[i]];
+    }
+  }
+
+
+  return result;
+};
+
+function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
