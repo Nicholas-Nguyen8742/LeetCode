22391a75a26a443b0a86581b6f945d205c0a31f5
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-07 20:52:07 -0600

    Time: 0 ms, Memory: 53.7 MB - LeetHub

---
+++ 0815-25-2379-minimum-recolors-to-get-k-consecutive-black-blocks/0815-25-2379-minimum-recolors-to-get-k-consecutive-black-blocks.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+var minimumRecolors = function(blocks, k) {
+  let left = 0, whitesCount = 0, recolorsCount = Infinity;
+
+  for (let right = 0; right < blocks.length; right++) {
+    if (blocks[right] === "W") {
+      whitesCount++;
+    }
+
+    if (right - left + 1 === k) {
+      recolorsCount = Math.min(recolorsCount, whitesCount);
+
+      if (blocks[left] === "W") {
+        whitesCount--;
+      }
+
+      left += 1;
+    }
+  }
+
+  return recolorsCount;
+};
