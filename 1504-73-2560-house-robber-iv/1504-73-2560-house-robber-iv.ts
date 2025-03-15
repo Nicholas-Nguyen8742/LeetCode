9bbd56828b878149b4e24bf9290c9f9ceb47b84b
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-15 15:57:10 -0500

    Time: 20 ms, Memory: 67.6 MB - LeetHub

---
+++ 1504-73-2560-house-robber-iv/1504-73-2560-house-robber-iv.ts
@@ -0,0 +1,27 @@
+function minCapability(nums: number[], k: number): number {
+  let minReward = 1;
+  let maxReward = Math.max(...nums);
+  const total = nums.length;
+
+  while (minReward < maxReward) {
+    const midReward = Math.floor((minReward + maxReward) / 2);
+
+    let possible = 0, index = 0;
+    while (index < total) {
+      if (nums[index] <= midReward) {
+        possible++;
+        index += 2;
+      } else {
+        index++;
+      }
+    }
+
+    if (possible >= k) {
+      maxReward = midReward;
+    } else {
+      minReward = midReward + 1;
+    }
+  }
+
+  return minReward;
+};
