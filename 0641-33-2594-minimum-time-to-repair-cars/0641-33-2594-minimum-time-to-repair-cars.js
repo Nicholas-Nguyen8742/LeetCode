cf6c97dcef7510f38550eb2df0aae3efa1951ca6
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-03-15 19:25:09 -0500

    Time: 66 ms, Memory: 66.3 MB - LeetHub

---
+++ 0641-33-2594-minimum-time-to-repair-cars/0641-33-2594-minimum-time-to-repair-cars.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} ranks
+ * @param {number} cars
+ * @return {number}
+ */
+var repairCars = function(ranks, cars) {
+  let low = 1, high = cars * cars * ranks[0];
+
+  while (low < high) {
+    const mid = Math.floor((low + high) / 2);
+    let repairedCars = 0;
+
+    for (const rank of ranks) {
+      repairedCars += Math.floor(Math.sqrt(Math.floor(mid / rank)));
+    }
+
+    if (repairedCars < cars) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
+};
