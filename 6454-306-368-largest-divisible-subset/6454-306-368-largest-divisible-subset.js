52cc1b51a96a626358b2c47fb45804ead67c9468
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-04-06 12:51:13 -0500

    Time: 35 ms, Memory: 56.5 MB - LeetHub

---
+++ 6454-306-368-largest-divisible-subset/6454-306-368-largest-divisible-subset.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var largestDivisibleSubset = function(nums) {
+  if (!nums.length) return [];
+
+  const n = nums.length;
+  nums.sort((a, b) => a - b);
+
+  const dp = new Array(n).fill(1);
+  const prevIndex = new Array(n).fill(-1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] % nums[j] === 0 && dp[i] < dp[j] + 1) {
+        dp[i] = dp[j] + 1;
+        prevIndex[i] = j;
+      }
+    }
+  }
+
+  let maxIndex = dp.indexOf(Math.max(...dp));
+
+  const result = [];
+  while (maxIndex !== -1) {
+    result.unshift(nums[maxIndex]);
+    maxIndex = prevIndex[maxIndex];
+  }
+
+  return result;
+};
