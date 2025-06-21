d895377b0cef25dbb772f0251dd6128fdc848d91
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-06-20 19:56:16 -0500

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 50 ms (27.59%), Space: 57.5 MB (5.74%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,14 +1,9 @@
-function combine(char: string, index: number, str: string, isWord1: boolean) {
-  return isWord1 ? [char, str[index]] : [str[index], char];
-}
-
-
 function mergeAlternately(word1: string, word2: string): string {
   const isWord1 = word1.length > word2.length;
   const larger = isWord1 ? word1 : word2;
   const other = isWord1 ? word2 : word1;
   return larger
     .split('')
-    .flatMap((char, index) => combine(char, index, other, isWord1))
+    .flatMap((char, index) => isWord1 ? [char, other[index]] : [other[index], char])
     .join('')
 };
