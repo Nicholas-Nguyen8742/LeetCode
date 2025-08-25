ca6cf5d806a4f523bdc5640b4061ceff898bc2e5
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-08-25 11:14:31 -0500

    0498-DIAGONAL-TRAVERSE Stats: Time: 13 ms (36.15%), Space: 69 MB (20.77%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.ts
@@ -0,0 +1,20 @@
+function findDiagonalOrder(mat: number[][]): number[] {
+  const m = mat.length, n = mat[0].length, map = new Map();
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      const key = i + j;
+      if (!map.has(key)) map.set(key, []);
+      map.get(key).push(mat[i][j]);
+    }
+  }
+
+  const result = [];
+  for (let k = 0; k < m + n - 1; k++) {
+    const list = map.get(k);
+    if (k % 2 === 0) list.reverse();
+    result.push(...list);
+  }
+
+  return result;
+};
