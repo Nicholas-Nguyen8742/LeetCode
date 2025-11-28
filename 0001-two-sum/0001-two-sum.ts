0866b3762b7d39acab9fd7a251b19a258ed5568a
Nicholas Nguyen <97898047+Nicholas-Nguyen8742@users.noreply.github.com>
2025-11-27 19:40:02 -0600

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 57.1 MB (47.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,5 +1,6 @@
 function twoSum(nums: number[], target: number): number[] {
   const map = new Map();
+
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
     if (map.has(complement)) {
